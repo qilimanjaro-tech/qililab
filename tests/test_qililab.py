ae732eeb2c0c8b7482035417307f87efc2bb96e0
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-03-24 11:49:21 +0100

    docs: :memo: Update docs on test functions

--- tests/test_qililab.py
+++ tests/test_qililab.py
@@ -4,11 +4,11 @@ from qililab import Qililab
 
 
 @pytest.fixture
-def qililab():
-    """_summary_
+def qililab() -> Qililab:
+    """Test that instantiates the Qililab class
 
     Returns:
-        _type_: _description_
+        Qililab: Qililab dummy object
     """
     return Qililab()
 
@@ -16,10 +16,10 @@ def qililab():
 class TestQililab:
     """Unitary tests checking the Qililab initialization steps and values"""
 
-    def test_qililab_constructor(self, qililab):
-        """_summary_
+    def test_qililab_constructor(self, qililab: Qililab) -> None:
+        """Test of the constructor of the Qililab class
 
         Args:
-            qililab (_type_): _description_
+            qililab (Qililab): Qililab dummy object
         """
         assert isinstance(qililab, Qililab)
