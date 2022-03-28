1b8574c94feb156b4a52e9d70e45bfa41a1f4e6e
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-03-28 12:18:13 +0200

    refactor: :recycle: Delete useless methods.

--- src/qililab/platforms/abstract_platform.py
+++ src/qililab/platforms/abstract_platform.py
@@ -8,7 +8,6 @@ class AbstractPlatform(ABC):
 
     Attributes:
         name (str): Name of the platform.
-
     """
 
     def __init__(self, name: str) -> None:
@@ -26,37 +25,3 @@ class AbstractPlatform(ABC):
             str: Name of the platform
         """
         return self.name
-
-    @abstractmethod
-    def connect(self) -> None:
-        """Connect to lab instruments using the specified calibration settings."""
-
-    @abstractmethod
-    def setup(self) -> None:
-        """Configure instruments using the specified calibration settings."""
-
-    @abstractmethod
-    def start(self) -> None:
-        """Turn on lab instruments."""
-
-    @abstractmethod
-    def stop(self) -> None:
-        """Turn off lab instruments."""
-
-    @abstractmethod
-    def disconnect(self) -> None:
-        """Disconnect from lab instruments."""
-
-    # TODO: Replace 'object' with PulseSequence class (when defined)
-    @abstractmethod
-    def execute(self, sequence: object, nshots: int = None):
-        """Execute a pulse sequence.
-
-        Args:
-            sequence (PulseSequence): Pulse sequence to execute.
-            nshots (int): Number of shots to sample from the experiment. If ``None`` the default
-            value provided as hardware_avg in the calibration json will be used.
-
-        Returns:
-            Readout results.
-        """
