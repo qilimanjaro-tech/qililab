3f0df3893e383984b7bc4ee444bc972bff69c199
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-03-28 17:57:35 +0200

    feat: :sparkles: Add PlatformSettings class

---
+++ src/qililab/settings/platform_settings.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from qililab.settings.abstract_settings import AbstractSettings
+
+
+@dataclass
+class PlatformSettings(AbstractSettings):
+    """Contains the settings of the platform.
+
+    Args:
+        n_qubits (int): Number of qubits.
+        hw_avg (int): Hardware average. Number of shots used when executing a sequence.
+        sw_avg (float): Software average.
+        sampling_rate (float): Minimum voltage obtained on a rabi oscillation.
+        rep_duration (float): Maximum voltage obtained on a rabi oscillation.
+    """
+
+    nqubits: int
+    hw_avg: int
+    sw_avg: int
