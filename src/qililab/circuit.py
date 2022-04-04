50deadf62807e5104300536278d750e6894c9544
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-04 13:22:58 +0200

    refactor: :recycle: Remove dataclass from HardwareCircuit

--- src/qililab/circuit.py
+++ src/qililab/circuit.py
@@ -1,20 +1,13 @@
-from dataclasses import dataclass
-
 from numpy import ndarray
-from qibo import K
 from qibo.core.circuit import Circuit
 
 
-@dataclass
 class HardwareCircuit(Circuit):
     """Class used to create circuit model.
 
     Args:
         nqubits (int): Number of qubits.
     """
-
-    def __init__(self, nqubits: int) -> None:
-        super().__init__(nqubits)
 
     def execute(self, initial_state: ndarray = None, nshots: int = None):
         """Executes a pulse sequence"""
