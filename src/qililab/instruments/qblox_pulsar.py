56138621a57e6708c8ec7f341926c1093c111ea4
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-05 17:45:13 +0200

    feat: :poop: Check connection before running class method.
    We should find a way to avoid running _check_connected() every time

--- src/qililab/instruments/qblox_pulsar.py
+++ src/qililab/instruments/qblox_pulsar.py
@@ -40,44 +40,59 @@ class QbloxPulsar(Instrument):
 
     def start(self):
         """Executes the uploaded instructions."""
+        # FIXME: Find a solution to check the connection before running all methods (except connect and load_settings)
+        # without having to call self._check_connected() every time.
+        self._check_connected()
         self.device.arm_sequencer()
         self.device.start_sequencer()
 
     def stop(self):
         """Stops the QBlox sequencer from sending pulses."""
+        self._check_connected()
         self.device.stop_sequencer()
 
     def close(self):
         """Disconnects from the instrument."""
-        if self._connected:
-            self.stop()
-            self.device.close()
-            self._connected = False
+        self._check_connected()
+        self.stop()
+        self.device.close()
+        self._connected = False
 
     def reset(self):
         """Reset instrument."""
+        self._check_connected()
         self.device.reset()
 
     def initial_setup(self):
         """Initial setup of the instrument."""
+        self._check_connected()
         self._set_reference_source()
         self._set_sync_enabled()
 
     def setup(self):
         """Sets Qblox instrument calibration settings."""
+        self._check_connected()
         self._set_gain()
 
     def _set_gain(self):
         """Set gain of sequencer for all paths."""
+        self._check_connected()
         sequencer = self.settings.sequencer
         getattr(self.device, f"sequencer{sequencer}_gain_awg_path0")(self.settings.gain)
         getattr(self.device, f"sequencer{sequencer}_gain_awg_path1")(self.settings.gain)
 
     def _set_reference_source(self):
         """Set reference source. Options are 'internal' or 'external'"""
+        self._check_connected()
         self.device.reference_source(self.settings.reference_clock)
 
     def _set_sync_enabled(self):
         """Enable/disable synchronization over multiple instruments."""
+        self._check_connected()
         sequencer = self.settings.sequencer
         getattr(self.device, f"sequencer{sequencer}_sync_en")(self.settings.sync_enabled)
+
+    def _check_connected(self):
+        """Raise AttributeError if the instrument is not connected."""
+        if not self._connected:
+            raise AttributeError("Instrument is not connected.")
--- src/qililab/instruments/qblox_pulsar_qrm.py
+++ src/qililab/instruments/qblox_pulsar_qrm.py
@@ -13,10 +13,12 @@ class QbloxPulsarQRM(QbloxPulsar):
 
     def _set_hardware_averaging(self):
         """Enable/disable hardware averaging of the data for all paths."""
+        self._check_connected()
         self.device.scope_acq_avg_mode_en_path0(self.settings.hardware_average_enabled)
         self.device.scope_acq_avg_mode_en_path1(self.settings.hardware_average_enabled)
 
     def _set_acquisition_mode(self):
         """Set scope acquisition trigger mode for all paths. Options are 'sequencer' or 'level'."""
+        self._check_connected()
         self.device.scope_acq_trigger_mode_path0(self.settings.acquire_trigger_mode)
         self.device.scope_acq_trigger_mode_path1(self.settings.acquire_trigger_mode)
