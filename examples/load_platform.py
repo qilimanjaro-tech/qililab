ceab8e2558e6c1a3f3202032e2f14c75195780eb
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-05 12:20:51 +0200

    chore: :memo: Add load_platform example using PLATFORM_BUILDER

--- examples/load_platform.py
+++ examples/load_platform.py
@@ -1,5 +1,7 @@
 import qibo
 
+from qililab import PLATFORM_BUILDER
+
 # FIXME: Need to add backend in qibo's profiles.yml file
 backend = {
     "name": "qililab",
@@ -13,8 +15,12 @@ qibo.K.profile["backends"].append(backend)
 
 def load_platform() -> None:
     """Load the platform 'platform_0' from the settings folder."""
+    # Using qibo (needed when using qibo circuits)
     qibo.set_backend(backend="qililab", platform="platform_0")
     print(f"Platform name: {qibo.K.platform}")
+    # Using PLATFORM_BUILDER
+    platform = PLATFORM_BUILDER.build(name="platform_0")
+    print(f"Platform name: {platform}")
 
 
 if __name__ == "__main__":
