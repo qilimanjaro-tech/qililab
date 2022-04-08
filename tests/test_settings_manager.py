b506a2fd57b4c755d84200f2bd8ff3f96935fac1
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-08 16:22:39 +0200

    style: :art: Remove useless return types

--- tests/test_settings_manager.py
+++ tests/test_settings_manager.py
@@ -9,27 +9,27 @@ from qililab.settings.settings_manager import SettingsManager
 class TestSettingsManager:
     """Unit tests checking the SettingsManager attributes and methods"""
 
-    def test_singleton(self) -> None:
+    def test_singleton(self):
         """Test that SettingsManager is a singleton."""
         settings_manager = SettingsManager(foldername="qili")
         assert id(settings_manager) == id(SETTINGS_MANAGER)
 
-    def test_load_default_platform_settings(self) -> None:
+    def test_load_default_platform_settings(self):
         """Test the load method of the SettingsManager class with the default platform settings.
         Assert that errors are raised correctly."""
         assert isinstance(SETTINGS_MANAGER.load(filename="platform"), PlatformSettings)
 
-    def test_load_default_qubit_settings(self) -> None:
+    def test_load_default_qubit_settings(self):
         """Test the load method of the SettingsManager class with the default qubit settings.
         Assert that errors are raised correctly."""
         assert isinstance(SETTINGS_MANAGER.load(filename="qubit_0"), QubitCalibrationSettings)
 
-    def test_load_unknown_file(self) -> None:
+    def test_load_unknown_file(self):
         """Test the load method of the SettingsManager class with an unknown file."""
         with pytest.raises(FileNotFoundError):
             SETTINGS_MANAGER.load(filename="unknown_file")
 
-    def test_dump_default_platform_settings(self) -> None:
+    def test_dump_default_platform_settings(self):
         """Test the dump method of the SettingsManager class."""
         settings = SETTINGS_MANAGER.load(filename="platform")
         SETTINGS_MANAGER.dump(settings)
