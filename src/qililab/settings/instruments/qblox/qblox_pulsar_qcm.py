15ff7bae8e7c56968d62568ac94181dc669b455a
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-12 15:01:06 +0200

    fix: :bug: Fix circular import

--- src/qililab/settings/instruments/qblox/qblox_pulsar_qcm.py
+++ src/qililab/settings/instruments/qblox/qblox_pulsar_qcm.py
@@ -1,7 +1,7 @@
 """Qblox pulsar QCM settings class"""
 from dataclasses import dataclass
 
-from qililab.settings import QubitControlSettings
+from qililab.settings.instruments.qubit_control import QubitControlSettings
 
 
 @dataclass
