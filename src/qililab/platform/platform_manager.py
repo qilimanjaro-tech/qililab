3464e4ec9c46d9b313432105e7453a8bdb28802d
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-25 14:22:38 +0200

    refactor: :recycle: Remove attributes from PlatformManager.
    Create SingletonABC class.

--- src/qililab/platform/platform_manager.py
+++ src/qililab/platform/platform_manager.py
@@ -4,34 +4,27 @@ from pathlib import Path
 
 import yaml
 
-from qililab.config import logger
 from qililab.constants import DEFAULT_PLATFORM_DUMP_FILENAME
 from qililab.platform.components.buses import Buses
 from qililab.platform.components.schema import Schema
 from qililab.platform.platform import Platform
+from qililab.typings import YAMLNames
+from qililab.utils import SingletonABC
 
 
-class PlatformManager(ABC):
+class PlatformManager(ABC, metaclass=SingletonABC):
     """Manager of platform objects."""
 
-    platform_name: str
-
-    def build(self, platform_name: str) -> Platform:
+    def build(self, **kwargs: str) -> Platform:
         """Build platform.
-
-        Args:
-            platform_name (str): Name of the platform.
 
         Returns:
             Platform: Platform object describing the setup used.
         """
-        logger.info("Building platform %s", platform_name)
-
-        self.platform_name = platform_name
-
-        schema = self._build_schema()
-        buses = self._build_buses(schema=schema)
-        return self._build_platform(schema=schema, buses=buses)
+        settings = self._load_settings(**kwargs)
+        schema = Schema(settings=settings[YAMLNames.SCHEMA.value])
+        buses = Buses(buses=schema.buses)
+        return Platform(settings=settings[YAMLNames.PLATFORM.value], schema=schema, buses=buses)
 
     def dump(self, platform: Platform):
         """Dump all platform information into a YAML file.
@@ -43,32 +36,10 @@ class PlatformManager(ABC):
         with open(file=file_path, mode="w", encoding="utf-8") as file:
             yaml.safe_dump(data=platform.to_dict(), stream=file, sort_keys=False)
 
-    def _build_platform(self, schema: Schema, buses: Buses) -> Platform:
-        """Build platform."""
-        platform_settings = self._load_platform_settings()
-        return Platform(settings=platform_settings, schema=schema, buses=buses)
-
-    def _build_schema(self) -> Schema:
-        """Build platform schema."""
-        schema_settings = self._load_schema_settings()
-        return Schema(settings=schema_settings)
-
-    def _build_buses(self, schema: Schema) -> Buses:
-        """Build platform buses."""
-        return Buses(schema.buses)
-
     @abstractmethod
-    def _load_platform_settings(self) -> dict:
-        """Load platform settings.
+    def _load_settings(self, **kwargs: str) -> dict:
+        """Load platform and schema settings.
 
         Returns:
-            dict: Dictionary with platform settings.
-        """
-
-    @abstractmethod
-    def _load_schema_settings(self) -> dict:
-        """Load schema settings.
-
-        Returns:
-            dict: Dictionary with schema settings.
+            dict: Dictionary with platform and schema settings.
         """
--- src/qililab/platform/platform_manager_db.py
+++ src/qililab/platform/platform_manager_db.py
@@ -10,22 +10,23 @@ from qililab.settings import SETTINGS_MANAGER
 class PlatformManagerDB(PlatformManager):
     """Manager of platform objects."""
 
-    def _load_platform_settings(self) -> dict:
-        """Load platform settings.
+    def _load_settings(self, **kwargs: str) -> dict:
+        """Load platform and schema settings.
+
+        Args:
+            platform_name (str): The name of the platform.
 
         Returns:
-            dict: Dictionary with platform settings.
+            dict: Dictionary with platform and schema settings.
         """
-        return SETTINGS_MANAGER.load(
-            foldername=DEFAULT_SETTINGS_FOLDERNAME, platform_name=self.platform_name, filename=DEFAULT_PLATFORM_FILENAME
-        )
-
-    def _load_schema_settings(self) -> dict:
-        """Load schema settings.
-
-        Returns:
-            dict: Dictionary with schema settings.
-        """
-        return SETTINGS_MANAGER.load(
-            foldername=DEFAULT_SETTINGS_FOLDERNAME, platform_name=self.platform_name, filename=DEFAULT_SCHEMA_FILENAME
-        )
+        if "platform_name" not in kwargs:
+            raise ValueError("Please provide a 'platform_name' argument.")
+        platform_name = kwargs["platform_name"]
+        return {
+            "platform": SETTINGS_MANAGER.load(
+                foldername=DEFAULT_SETTINGS_FOLDERNAME, platform_name=platform_name, filename=DEFAULT_PLATFORM_FILENAME
+            ),
+            "schema": SETTINGS_MANAGER.load(
+                foldername=DEFAULT_SETTINGS_FOLDERNAME, platform_name=platform_name, filename=DEFAULT_SCHEMA_FILENAME
+            ),
+        }
