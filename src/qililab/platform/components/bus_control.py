878d4cf1746675d4789758b1e3c743424327da9d
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-04-29 09:58:45 +0200

    feat: :sparkles: Add BusControl and BusReadout classes.

---
+++ src/qililab/platform/components/bus_control.py
@@ -0,0 +1,41 @@
+"""BusControl class."""
+from dataclasses import dataclass, field
+
+from qililab.instruments import QubitControl
+from qililab.platform.components.bus import Bus
+from qililab.platform.components.qubit import Qubit
+
+
+@dataclass
+class BusControl(Bus):
+    """BusControl class. This bus contains a qubit control and a signal generator, which are connected
+    through a mixer for up-conversion, and a qubit at the end of the bus.
+
+    Args:
+        settings (BusControlSettings): Bus settings.
+    """
+
+    @dataclass
+    class BusControlSettings(Bus.BusSettings):
+        """BusSettings class.
+        Args:
+            qubit_control (QubitControl): Class containing the instrument used for control of the qubits.
+            qubit (Qubit): Class containing the qubit object.
+        """
+
+        qubit: Qubit = field(init=False)
+        qubit_instrument: QubitControl = field(init=False)
+
+    settings: BusControlSettings
+
+    def __init__(self, settings: dict):
+        self.settings = self.BusControlSettings(**settings)
+
+    @property
+    def qubit(self):
+        """Bus 'qubit' property.
+
+        Returns:
+            Resonator: settings.qubit.
+        """
+        return self.settings.qubit
---
+++ src/qililab/platform/components/bus_readout.py
@@ -0,0 +1,70 @@
+"""BusReadout class."""
+from dataclasses import dataclass, field
+
+from qililab.instruments import Mixer, QubitReadout
+from qililab.platform.components.bus import Bus
+from qililab.platform.components.resonator import Resonator
+from qililab.typings import Category
+
+
+@dataclass
+class BusReadout(Bus):
+    """BusReadout class. This bus contains a qubit readout and a signal generator, which are connected
+    through a mixer for up-conversion. The bus also contains a resonator, which should be connected to one or multiple qubits.
+    We finally have another mixer used for down-conversion.
+
+    Args:
+        settings (BusReadoutSettings): Bus settings.
+    """
+
+    @dataclass
+    class BusReadoutSettings(Bus.BusSettings):
+        """BusSettings class.
+        Args:
+            qubit_readout (QubitReadout): Class containing the instrument used for readout of the qubits.
+            signal_generator (SignalGenerator): Class containing the signal generator instrument.
+            mixer_up (Mixer): Class containing the mixer object, used for up-conversion.
+            mixer_down (Mixer): Class containing the mixer object, used for down-conversion.
+            resonator (Resonator): Class containing the resonator object.
+        """
+
+        mixer_down: Mixer = field(init=False)
+        resonator: Resonator = field(init=False)
+        qubit_instrument: QubitReadout = field(init=False)
+
+    settings: BusReadoutSettings
+
+    def __init__(self, settings: dict):
+        self.settings = self.BusReadoutSettings(**settings)
+
+    @property
+    def mixer_down(self):
+        """Bus 'mixer' property.
+
+        Returns:
+            Mixer: settings.mixer.
+        """
+        return self.settings.mixer_down
+
+    @property
+    def resonator(self):
+        """Bus 'resonator' property.
+
+        Returns:
+            Resonator: settings.resonator.
+        """
+        return self.settings.resonator
+
+    def get_element(self, category: Category, id_: int):
+        """Get bus element. Return None if element is not found.
+
+        Args:
+            category (str): Category of element.
+            id_ (int): ID of element.
+
+        Returns:
+            (QubitReadout | SignalGenerator | Mixer | Resonator | Qubit | None): Element class.
+        """
+        if category == Category.QUBIT:
+            return self.resonator.get_qubit(id_=id_)
+        return super().get_element(category=category, id_=id_)
