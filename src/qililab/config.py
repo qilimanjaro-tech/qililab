1ad6e7844dcbe780c9bec4c50761636a0039139d
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-05-24 16:03:53 +0200

    style: :art: Move import.

--- src/qililab/config.py
+++ src/qililab/config.py
@@ -1,12 +1,11 @@
 """config.py"""
-# global configuration settings
 import logging
 import os
 
+from qililab import __version__
+
 # Logging level from 0 (all) to 4 (errors) (see https://docs.python.org/3/library/logging.html#logging-levels)
 LIBRARY_LOG_LEVEL = int(os.environ.get("LIBRARY_LOG_LEVEL", 1))
-
-# Configuration for logging mechanism
 
 
 class CustomHandler(logging.StreamHandler):
@@ -14,13 +13,11 @@ class CustomHandler(logging.StreamHandler):
 
     def format(self, record):
         """Format the record with specific format."""
-        from qililab import __version__
 
         fmt = f"[qililab] [{__version__}|%(levelname)s|%(asctime)s]: %(message)s"
         return logging.Formatter(fmt, datefmt="%Y-%m-%d %H:%M:%S").format(record)
 
 
-# allocate logger object
 logger = logging.getLogger(__name__)
 logger.setLevel(LIBRARY_LOG_LEVEL)
 logger.addHandler(CustomHandler())
