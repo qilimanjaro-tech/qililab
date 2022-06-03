17807c922d210155e9ab962f89a9cadd190308f6
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-06-03 12:24:18 +0200

    fix: :bug: Small fix.

--- src/qililab/typings/settings.py
+++ src/qililab/typings/settings.py
@@ -7,4 +7,6 @@ class SettingsType:
     def set_parameter(self, name: str, value: float | str | bool):
         """Cast the new value to its corresponding type and set the new attribute."""
         attr_type = type(getattr(self, name))
+        if attr_type == int:  # FIXME: Depending on how we define de value, python thinks it is an int
+            attr_type = float
         setattr(self, name, attr_type(value))
