e28075fd819b38462e043ad3bac8915935c559dc
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-06-17 13:45:53 +0200

    fix: :bug: Small fix.

--- src/qililab/instruments/keithley/keithley_2600.py
+++ src/qililab/instruments/keithley/keithley_2600.py
@@ -2,14 +2,14 @@
 import xarray
 
 from qililab.instruments.instrument import Instrument
-from qililab.typings import BusElementName, Keithley2600Driver
+from qililab.typings import InstrumentName, Keithley2600Driver
 from qililab.utils import nested_dataclass
 
 
 class Keithley2600(Instrument):
     """Keithley2600 class."""
 
-    name = BusElementName.KEITHLEY
+    name = InstrumentName.KEITHLEY
 
     @nested_dataclass
     class Keithley2600Settings(Instrument.InstrumentSettings):
@@ -20,10 +20,6 @@ class Keithley2600(Instrument):
 
     settings: Keithley2600Settings
     device: Keithley2600Driver
-
-    def __init__(self, settings: dict):
-        super().__init__()
-        self.settings = self.Keithley2600Settings(**settings)
 
     @Instrument.CheckConnected
     def setup(self):
