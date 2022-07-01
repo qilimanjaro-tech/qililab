1bece72bcdb0f445cd79ed9f3e3a4f438d1d597a
amitjansc <albert.mitjans@qilimanjaro.tech>
2022-07-01 10:42:06 +0200

    style: :art: Remove import *.

--- src/qililab/pulse/__init__.py
+++ src/qililab/pulse/__init__.py
@@ -2,6 +2,6 @@
 from .circuit_to_pulses import CircuitToPulses
 from .pulse import Pulse
 from .pulse_sequence import PulseSequence
-from .pulse_shape import *
+from .pulse_shape import Drag, Gaussian, PulseShape, Rectangular
 from .pulses import Pulses
 from .readout_pulse import ReadoutPulse
