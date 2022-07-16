ceb31aa263e1a1f46b2129108113593f2ff54d3a
Albert Solana <iamtxena@gmail.com>
2022-07-16 09:09:44 +0200

    chore:

--- src/qililab/instruments/qblox/qblox_qcm.py
+++ src/qililab/instruments/qblox/qblox_qcm.py
@@ -1,7 +1,6 @@
 """Qblox QCM class"""
 from dataclasses import dataclass
 
-from qililab.instruments.instrument import Instrument
 from qililab.instruments.qblox.qblox_module import QbloxModule
 from qililab.instruments.qubit_control import QubitControl
 from qililab.instruments.utils.instrument_factory import InstrumentFactory
@@ -23,8 +22,3 @@ class QbloxQCM(QbloxModule, QubitControl):
         """Contains the settings of a specific pulsar."""
 
     settings: QbloxQCMSettings
-
-    @Instrument.CheckDeviceInitialized
-    def reset(self):
-        """Reset instrument."""
-        self.device.reset()
