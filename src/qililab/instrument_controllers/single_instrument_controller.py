34afb16328ddb878282e52aa2cde259a94d4c740
Albert Solana <iamtxena@gmail.com>
2022-07-16 09:09:19 +0200

    refactor: setting a device with a propper way

--- src/qililab/instrument_controllers/single_instrument_controller.py
+++ src/qililab/instrument_controllers/single_instrument_controller.py
@@ -37,4 +37,4 @@ class SingleInstrumentController(InstrumentController):
         For a SingleInstrumentController, by default,
         the same controller device driver is set to the Instrument.
         """
-        setattr(self.modules[0], "device", self.device)
+        self.modules[0].device = self.device
