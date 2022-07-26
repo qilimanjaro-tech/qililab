58296ad110a486ebdae38cc32665df490e8883f0
Albert Solana <iamtxena@gmail.com>
2022-07-26 20:00:21 +0200

    test: :test_tube: add num sequences parameter

--- tests/integration/utils/test_live_plot.py
+++ tests/integration/utils/test_live_plot.py
@@ -34,13 +34,13 @@ class TestLivePlot:
 
     def test_live_plot_instance_no_connection(self, valid_remote_api: RemoteAPI):
         """Test that the LivePlot instance is created correctly without any connection"""
-        plot = LivePlot(remote_api=valid_remote_api)
+        plot = LivePlot(remote_api=valid_remote_api, num_sequences=1)
         assert isinstance(plot, LivePlot)
 
     def test_live_plot_ranges_with_one_loop(self, valid_remote_api: RemoteAPI, one_loop: Loop):
         """test live plot ranges with one loop"""
 
-        plot = LivePlot(remote_api=valid_remote_api, loops=[one_loop])
+        plot = LivePlot(remote_api=valid_remote_api, loops=[one_loop], num_sequences=1)
         for x_value in one_loop.range:
             plot.send_points(value=x_value**2)
             time.sleep(0.1)
@@ -48,7 +48,7 @@ class TestLivePlot:
     def test_live_plot_ranges_with_two_loops(self, valid_remote_api: RemoteAPI, one_loop: Loop):
         """test live plot ranges with two loops"""
         loop = Loop(parameter=Parameter.GAIN, start=1, stop=11, num=10, loop=one_loop)
-        plot = LivePlot(remote_api=valid_remote_api, loops=[loop])
+        plot = LivePlot(remote_api=valid_remote_api, loops=[loop], num_sequences=1)
         for x_value in loop.range:
             for y_value in one_loop.range:
                 z_value = float(np.sin(x_value * y_value / (2 * np.pi)))
