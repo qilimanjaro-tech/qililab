17c81e3ff439576a0b40962536105599ca641006
JavierSab <66302375+JavierSab@users.noreply.github.com>
2022-08-17 19:00:19 +0200

    Feat/qili 173 serialize results (#43)
    * feat(results): serializing
    
    introduced functionality to serialize Results to dictionaries
    
    * feat(results): serializing
    
    introduced functionality to serialize Results to dictionaries
    
    * feat(results): serializing
    
    added a from_dict() method to Results for more clarity, as now it is going to be a class that the user is going to be using directly. Imported that class into main __init__ so it is clearly accessible
    
    Co-authored-by: javi <javier.sabariego@qilimanjaro.tech>

--- src/qililab/__init__.py
+++ src/qililab/__init__.py
@@ -3,4 +3,5 @@
 from .config import __version__, logger
 from .experiment import Experiment
 from .platform import build_platform, save_platform
+from .result import Results
 from .utils.load_data import load
