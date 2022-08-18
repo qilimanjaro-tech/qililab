5c0318291cf97ae1d520e53ce26d4ef1a37001c2
Albert Solana <iamtxena@gmail.com>
2022-08-18 09:02:36 +0200

    fix: :bug: uses deepcopy before pop dict key

--- tests/unit/experiment/test_results.py
+++ tests/unit/experiment/test_results.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+""" Test Results """
 
 import pytest
 
@@ -10,15 +10,14 @@ from ...data import results_one_loops, results_two_loops
 class TestsResults:
     """Test `Results` functionalities."""
 
-    @pytest.mark.parametrize("results_dict", deepcopy([results_one_loops, results_two_loops]))
+    @pytest.mark.parametrize("results_dict", [results_one_loops, results_two_loops])
     def test_from_dict_method(self, results_dict: dict):
         """Tests from_dict() serialization of results gives out a valid Results instance."""
         results = Results.from_dict(results_dict)
         assert isinstance(results, Results)
 
-    @pytest.mark.parametrize("results_dict", deepcopy([results_one_loops, results_two_loops]))
+    @pytest.mark.parametrize("results_dict", [results_one_loops, results_two_loops])
     def test_to_dict_method(self, results_dict: dict):
         """Tests to_dict() serialization of results gives the intended dictionary."""
-        results_original = deepcopy(results_dict)
         results_final = Results.from_dict(results_dict).to_dict()
-        assert results_final == results_original
+        assert results_final == results_dict
