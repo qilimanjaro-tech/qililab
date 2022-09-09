2b7079c7657fb122c571f47865db8c631c75a5b8
rsagas <rsagas@qilimanjaro.tech>
2022-09-09 12:05:44 +0000

    Added a better docstring

--- src/qililab/remote_connection/remote_api.py
+++ src/qililab/remote_connection/remote_api.py
@@ -22,13 +22,24 @@ class RemoteAPI:
             self.device_id = GALADRIEL_DEVICE_ID
 
     def __enter__(self):
-        """Code executed when starting a with statement."""
+        """Code executed when starting a with statement.
+        If:
+            1. there is a connection and,
+            2. this control has not been overriden,
+        Then it blocks the device.
+        """
         if (self.connection is not None) and (not self.manual_override):
             self.connection.block_device_id(device_id=self.device_id)
             self._blocked_device = True
 
     def __exit__(self, exc_type, exc_value, traceback):
-        """Code executed when stopping a with statement."""
+        """Code executed when stopping a with statement.
+        If:
+            1. there is a connection,
+            2. the device is blocked and,
+            3. this control has not been overriden,
+        Then it unblocks the device.
+        """
         if (self.connection is not None and self._blocked_device) and (not self.manual_override):
             self.connection.release_device(device_id=self.device_id)
             self._blocked_device = False
