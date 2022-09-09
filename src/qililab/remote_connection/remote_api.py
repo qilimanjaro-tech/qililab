58096d518e89a53a2b360aed9443a82cc4b608c6
rsagas <rsagas@qilimanjaro.tech>
2022-09-09 12:03:54 +0000

    Added a manual override for user to neglect the device control

--- src/qililab/remote_connection/remote_api.py
+++ src/qililab/remote_connection/remote_api.py
@@ -13,6 +13,7 @@ class RemoteAPI:
     connection: API | None = field(default=None)
     device_id: int | None = field(default=GALADRIEL_DEVICE_ID)
     _blocked_device: bool = field(init=False)
+    manual_override: bool = field(init=False)
 
     def __post_init__(self):
         """Post initial initialization"""
@@ -22,12 +23,12 @@ class RemoteAPI:
 
     def __enter__(self):
         """Code executed when starting a with statement."""
-        if self.connection is not None:
+        if (self.connection is not None) and (not self.manual_override):
             self.connection.block_device_id(device_id=self.device_id)
             self._blocked_device = True
 
     def __exit__(self, exc_type, exc_value, traceback):
         """Code executed when stopping a with statement."""
-        if self.connection is not None and self._blocked_device:
+        if (self.connection is not None and self._blocked_device) and (not self.manual_override):
             self.connection.release_device(device_id=self.device_id)
             self._blocked_device = False
