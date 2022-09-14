863764170c0317815d31b08f6a3617a8130cf1bc
Albert Solana <iamtxena@gmail.com>
2022-09-14 10:13:12 +0200

    feat: skip already formatted results

--- examples/format_results.py
+++ examples/format_results.py
@@ -3,13 +3,36 @@ import argparse
 import glob
 import os
 from pathlib import Path
+from typing import List
 
 from qililab import load
+from qililab.constants import EXPERIMENT_FILENAME, RESULTS_FILENAME
 from qililab.utils.load_data import update_results_files_format
 
 os.environ["DATA"] = str(Path(__file__).parent / "data")
 
 DEFAULT_PATH = "./examples/data/"
+RESULTS_FILENAME_BACKUP = "results_bak.yml"
+EXPERIMENT_FILENAME_BACKUP = "experiment_bak.yml"
+
+
+def check_string_included_in_list_strings(text: str, words: List[str]) -> bool:
+    """check if a string is included in a list of strings"""
+    return any(text in word for word in words)
+
+
+def files_already_formatted(directory: str) -> bool:
+    """check if the files inside the given directory have already been formatted"""
+    files = glob.glob(os.path.join(Path(directory), "*"))
+    if (
+        check_string_included_in_list_strings(text=RESULTS_FILENAME, words=files)
+        and check_string_included_in_list_strings(text=RESULTS_FILENAME_BACKUP, words=files)
+        and check_string_included_in_list_strings(text=EXPERIMENT_FILENAME, words=files)
+        and check_string_included_in_list_strings(text=EXPERIMENT_FILENAME_BACKUP, words=files)
+    ):
+        print(f"Skipping {directory}")
+        return True
+    return False
 
 
 def format_results(path: str):
@@ -22,12 +45,14 @@ def format_results(path: str):
         if not files_or_directories or len(files_or_directories) <= 0:
             raise ValueError("No previous results data found.")
         if not os.path.isdir(files_or_directories[0]):
-            print(f"Formating {directory}")
-            update_results_files_format(path=directory)
+            if not files_already_formatted(directory=directory):
+                print(f"Formating {directory}")
+                update_results_files_format(path=directory)
             continue
         for nested_directory in files_or_directories:
-            print(f"Formating {nested_directory}")
-            update_results_files_format(path=nested_directory)
+            if not files_already_formatted(directory=nested_directory):
+                print(f"Formating {nested_directory}")
+                update_results_files_format(path=nested_directory)
 
 
 def load_one_result(path: str):
@@ -66,6 +91,6 @@ if __name__ == "__main__":
         help="Relative path from where the executable is run",
     )
     args = parser.parse_args()
-    path: str = args.path if "path" in args else DEFAULT_PATH
-    format_results(path=path)
-    load_results(path=path)
+    input_path: str = args.path if "path" in args and args.path is not None else DEFAULT_PATH
+    format_results(path=input_path)
+    # load_results(path=input_path)
