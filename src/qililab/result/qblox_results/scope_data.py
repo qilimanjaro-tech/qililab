0adbb84b2390f0cb701601842ddc02b235d6c7e0
Albert Solana <iamtxena@gmail.com>
2022-09-21 11:31:01 +0200

    fix: checking for correct dictionary key in scope results

--- src/qililab/result/qblox_results/scope_data.py
+++ src/qililab/result/qblox_results/scope_data.py
@@ -22,7 +22,9 @@ class ScopeData:
     def __post_init__(self):
         """Change invalid name and cast to PathData class."""
         if isinstance(self.path0, dict) and isinstance(self.path1, dict):
-            self.path0["out_of_range"] = self.path0.pop("out-of-range")
+            if "out-of-range" in self.path0:  # pylint: disable=unsupported-membership-test
+                self.path0["out_of_range"] = self.path0.pop("out-of-range")
             self.path0 = self.PathData(**self.path0)  # pylint: disable=not-a-mapping
-            self.path1["out_of_range"] = self.path1.pop("out-of-range")
+            if "out-of-range" in self.path1:  # pylint: disable=unsupported-membership-test
+                self.path1["out_of_range"] = self.path1.pop("out-of-range")
             self.path1 = self.PathData(**self.path1)  # pylint: disable=not-a-mapping
