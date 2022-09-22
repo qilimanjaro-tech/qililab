8c5ae3f302573cc95a5298cb1ec456f097d93f13
rsagas <76485431+rsagas@users.noreply.github.com>
2022-09-22 19:14:58 +0200

    Testing formatting for code-quality test

--- src/qililab/typings/enums.py
+++ src/qililab/typings/enums.py
@@ -235,6 +235,7 @@ class SystemControlSubcategory(Enum):
         * simulated_system_control
         * heterodyne_system_control
     """
+    
     HETERODYNE_SYSTEM_CONTROL = "heterodyne_system_control"
     MIXER_BASED_SYSTEM_CONTROL = "mixer_based_system_control"
     SIMULATED_SYSTEM_CONTROL = "simulated_system_control"
