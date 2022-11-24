030e4946a34dea1703a29aad17d8f2731887e664
javi <javier.sabariego@qilimanjaro.tech>
2022-11-24 11:23:23 +0100

    fix(QbloxModule): updated use of `qpysequence` loops
    Now, it is assumed that qpysequence loops count back from N to 0. Changed uses of `Loop(name="name", begin=0, end=N)` to `Loop(name="name", begin=N)`.
    Except for the name='binning' case, where we force `Loop(name="binning", begin=0, end=N, step=1)`.

--- src/qililab/instruments/qblox/qblox_module.py
+++ src/qililab/instruments/qblox/qblox_module.py
@@ -119,8 +119,8 @@ class QbloxModule(AWG):
         """
         # Define program's blocks
         program = Program()
-        bin_loop = Loop(name="binning", begin=0, end=int(self.num_bins))
-        avg_loop = Loop(name="average", begin=0, end=nshots)
+        bin_loop = Loop(name="binning", begin=0, end=int(self.num_bins), step=1)
+        avg_loop = Loop(name="average", begin=nshots)
         bin_loop.append_block(block=avg_loop, bot_position=1)
         stop = Block(name="stop")
         stop.append_component(Stop())
