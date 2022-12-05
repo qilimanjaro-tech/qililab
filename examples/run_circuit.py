27cbbdc0096f99bc7d1a5012fbe48549b5c20d31
iamtxena <iamtxena@gmail.com>
2022-12-05 12:08:05 +0100

    docs: Added new examples

--- examples/run_circuit.py
+++ examples/run_circuit.py
@@ -5,78 +5,58 @@ from pathlib import Path
 from qibo.core.circuit import Circuit
 from qibo.gates import M, X
 from qiboconnection.api import API
-from qiboconnection.connection import ConnectionConfiguration
 
-from qililab import build_platform
-from qililab.experiment import Experiment
-from qililab.typings import Parameter
-from qililab.typings.execution import ExecutionOptions
-from qililab.typings.experiment import ExperimentOptions
+from qililab import Experiment, ExperimentOptions, ExperimentSettings, build_platform
+from qililab.typings.enums import Parameter
 from qililab.typings.loop import LoopOptions
-from qililab.utils import Loop
+from qililab.utils.loop import Loop
 
 os.environ["RUNCARDS"] = str(Path(__file__).parent / "runcards")
 os.environ["DATA"] = str(Path(__file__).parent / "data")
 
+SAURON_SOPRANO = 15
+
 
 def run_circuit(connection: API | None = None):
     """Load the platform 'galadriel' from the DB."""
-    platform = build_platform(name="template_base_runcard")
-    print(platform)
+    runcard_name = "sauron_soprano"
+    platform = build_platform(name=runcard_name)
+
+    platform.connect_and_set_initial_setup(automatic_turn_on_instruments=True)
 
     circuit = Circuit(1)
     circuit.add(X(0))
     circuit.add(M(0))
 
-    # loop = Loop(
-    #     alias="D5a",
-    #     parameter=Parameter.VOLTAGE,
-    #     options=LoopOptions(start=1.0, stop=1.5, num=10, channel_id=0),
-    # )
-    loop = Loop(
-        alias="S4g",
-        parameter=Parameter.CURRENT,
-        options=LoopOptions(start=0.0, stop=1.5, num=10, channel_id=3),
-    )
-    # inner_loop = Loop(
-    #     alias="bus_control",
-    #     parameter=Parameter.GAIN,
-    #     options=LoopOptions(start=0.5, stop=1.0, num=5, channel_id=0),
-    # )
-    # loop = Loop(
-    #     alias="bus_control",
-    #     parameter=Parameter.FREQUENCIES,
-    #     options=LoopOptions(start=1.0e08, stop=1.5e08, num=10, channel_id=0),
-    #     loop=inner_loop,
-    # )
-    # loop = Loop(
-    #     alias="bus_control",
-    #     parameter=Parameter.FREQUENCIES,
-    #     options=LoopOptions(start=1.0e08, stop=1.5e08, num=10, channel_id=0),
-    # )
-    # loop = Loop(
-    #     alias="bus_control",
-    #     parameter=Parameter.FREQUENCY,
-    #     options=LoopOptions(start=6.0e09, stop=6.5e09, num=10, channel_id=0),
-    # )
-
-    experiment = Experiment(
-        platform=platform,
-        schedules=circuit,
-        options=ExperimentOptions(
-            loops=[loop],
-            name="experiment_demo",
-            execution_options=ExecutionOptions(
-                set_initial_setup=False,
-                automatic_connect_to_instruments=False,
-                automatic_disconnect_to_instruments=False,
-                automatic_turn_on_instruments=False,
-                automatic_turn_off_instruments=False,
-            ),
-        ),
+    lo_freq_loop = Loop(
+        alias="drive_line_bus",
+        parameter=Parameter.LO_FREQUENCY,
+        options=LoopOptions(start=6.0e09, stop=6.5e09, num=10),
     )
 
-    results = experiment.execute()
+    settings = ExperimentSettings(
+        hardware_average=1000,
+        repetition_duration=200_000,
+        software_average=1,
+    )
+
+    options = ExperimentOptions(
+        loops=[lo_freq_loop],  # loops to run the experiment
+        settings=settings,  # experiment settings
+        connection=connection,  # remote connection for live plotting
+        device_id=SAURON_SOPRANO,  # device identifier to block and release for remote executions
+        name="experiment_name",  # name of the experiment (it will be also used for the results folder name)
+        plot_y_label=None,  # plot y-axis label
+        remote_device_manual_override=False,  # whether to block the remote device manually
+    )
+
+    sample_experiment = Experiment(
+        platform=platform,  # platform to run the experiment
+        circuits=[circuit],  # circuits to run the experiment
+        options=options,  # experiment options
+    )
+
+    results = sample_experiment.execute()
     print(results)
     print(results.acquisitions())
 
