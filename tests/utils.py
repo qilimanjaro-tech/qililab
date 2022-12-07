bfd92795e0909f175aae0abc7389d7a3a6dfa0cf
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2022-12-07 11:49:00 +0100

    Use lists in helper methods and add docstrings to tests

--- tests/utils.py
+++ tests/utils.py
@@ -4,8 +4,6 @@ from typing import List, Tuple
 import numpy as np
 import numpy.typing as npt
 
-from qililab.result.qblox_results.scope_data import ScopeData
-
 
 def name_generator(base: str):
     """Unique name generator
@@ -21,21 +19,38 @@ def name_generator(base: str):
 
 
 def compare_pair_of_arrays(
-    pair_a: Tuple[npt.NDArray[np.float32] | List[float], npt.NDArray[np.float32] | List[float]],
-    pair_b: Tuple[npt.NDArray[np.float32] | List[float], npt.NDArray[np.float32] | List[float]],
+    pair_a: Tuple[List[float], List[float]],
+    pair_b: Tuple[List[float], List[float]],
     tolerance: float,
 ) -> bool:
+    """Compares two pairs of arrays of the same length up to a given tolerance.
+
+    Args:
+        pair_a (Tuple[List[float], List[float]]): First pair of arrays.
+        pair_b (Tuple[List[float], List[float]]): Second pair of arrays.
+        tolerance (float): Absolute amount up to which the arrays can differ to be considered equal.
+
+    Returns:
+        bool: True if the arrays are equal up to the given tolerance, False otherwise.
+    """
     path0_ok = all(np.isclose(pair_a[0], pair_b[0], atol=tolerance))
     path1_ok = all(np.isclose(pair_a[1], pair_b[1], atol=tolerance))
     return path0_ok and path1_ok
 
 
-def complete_array(
-    array: npt.NDArray[np.float32] | List[float], filler: float, final_length: int
-) -> npt.NDArray[np.float32]:
-    array = np.array(array) if isinstance(array, List) else array
-    filler_list = np.ones(final_length - len(array)) * filler
-    return np.append(array, filler_list)
+def complete_array(array: List[float], filler: float, final_length: int) -> List[float]:
+    """Fills a given array with the given float as a filler up to the final_length specified.
+
+    Args:
+        array (List[float]): Original array.
+        filler (float): Number to use as a filler.
+        final_length (int): Final length of the array.
+
+    Returns:
+        List[float]: List of length `final_length` where the first `len(array)` elements are those of the original
+            array, and the remaining elements are are repetitions of `filler`.
+    """
+    return array + [filler] * (final_length - len(array))
 
 
 dummy_qrm_name_generator = name_generator("dummy_qrm")
