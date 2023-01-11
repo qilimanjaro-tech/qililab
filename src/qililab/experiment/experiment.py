649d204ead33df648cc420f65991d63995c0ac15
paulqili <paulqili@gmail.com>
2023-01-11 11:22:12 +0100

    skeleton of modifications wanted

--- src/qililab/experiment/experiment.py
+++ src/qililab/experiment/experiment.py
@@ -132,9 +132,9 @@ class Experiment:
             )
 
         with self._execution:
-            self._execution.setup() # beware it is not yet implemented
+            self._execution.setup() # beware it is not yet implemented                   
             # generate_waveforms should be contained in that CALL
-            self._execute_all_circuits_or_schedules()
+            self._execute_all_circuits_or_schedules()# This is a loop that will trigger the awg to start the already uploaded programs
 
         return self._results
 
