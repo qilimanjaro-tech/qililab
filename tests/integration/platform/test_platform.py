a3e6c958dbea258957e28944618d0b7a8ae7f383
JavierSab <66302375+JavierSab@users.noreply.github.com>
2023-01-18 23:32:35 +0100

    fix(tests): mypy complains
    ignored file where mypy does not realise a mock attribute that should also be a mock

--- tests/integration/platform/test_platform.py
+++ tests/integration/platform/test_platform.py
@@ -30,7 +30,7 @@ class TestPlatform:
         mocked_remote_connection.save_runcard.return_value = 0
         remote_api = RemoteAPI(connection=mocked_remote_connection, device_id=0)
         save_platform(platform=platform, database=True, remote_api=remote_api, description="TEST")
-        remote_api.connection.save_runcard.assert_called()
+        remote_api.connection.save_runcard.assert_called()  # type: ignore
 
     @patch("qililab.remote_connection.remote_api.RemoteAPI.connection")
     @patch("yaml.safe_load")
