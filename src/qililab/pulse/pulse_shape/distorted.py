working distorted pulse. need to fix normalization

src/qililab/pulse/pulse_shape/distorted.py
@@ -4,8 +4,10 @@ from dataclasses import dataclass
 import numpy as np
 from scipy import signal
 
+from qililab.constants import RUNCARD
 from qililab.pulse.pulse_shape.pulse_shape import PulseShape
 from qililab.typings import PulseShapeName
+from qililab.typings.enums import PulseShapeSettingsName
 from qililab.utils import Factory
 
 
@@ -14,9 +16,10 @@ from qililab.utils import Factory
 class Distorted(PulseShape):
     """Distorted/square pulse shape."""
 
-    name: PulseShapeName = PulseShapeName.DISTORTED
+    name = PulseShapeName.DISTORTED
+    tau: float
 
-    def envelope(self, duration: int, amplitude: float, tau: float, resolution: float = 1.0):
+    def envelope(self, duration: int, amplitude: float, resolution: float = 1.0):
         """Distorted square envelope.
 
         Args:
@@ -27,8 +30,22 @@ class Distorted(PulseShape):
             ndarray: Amplitude of the envelope for each time step.
         """
         ysig = amplitude * np.ones(round(duration / resolution))
-        k = 2 * tau
+        k = 2 * self.tau
         a = [1, -1]
         b = [(k + 1) / k, -(k - 1) / k]
+        
+        test = signal.lfilter(b, a, ysig)/(a[0]**(duration / resolution) * b[0]**(duration / resolution)+0.1)
 
-        return signal.lfilter(b, a, ysig)
+        print(b[0]**(duration / resolution))
+        return signal.lfilter(b, a, ysig)/(a[0]**(duration / resolution) * b[0]**(duration / resolution)+0.1)
+
+    def to_dict(self):
+        """Return dictionary representation of the pulse shape.
+
+        Returns:
+            dict: Dictionary.
+        """
+        return {
+            RUNCARD.NAME: self.name.value,
+            PulseShapeSettingsName.TAU.value: self.tau,
+        }