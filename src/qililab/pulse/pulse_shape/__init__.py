2648c9c55067701fa8dd90798c194c9f81b1b1c2
andrea-pema <andrea-pema@gmail.com>
2023-03-17 13:04:32 +0100

    bias tee correction updated

--- src/qililab/pulse/pulse_shape/__init__.py
+++ src/qililab/pulse/pulse_shape/__init__.py
@@ -3,4 +3,4 @@ from .drag import Drag
 from .gaussian import Gaussian
 from .pulse_shape import PulseShape
 from .rectangular import Rectangular
-from .distorted import BiasTeeCorrection
+from .bias_tee_correction import BiasTeeCorrection
