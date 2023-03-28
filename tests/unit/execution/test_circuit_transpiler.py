c8547d1527762d1127b992c889a8fad64953d53e
Vyron Vasileiadis <hi@fedonman.com>
2023-03-28 16:39:46 +0200

    updates tests

--- tests/unit/execution/test_circuit_transpiler.py
+++ tests/unit/execution/test_circuit_transpiler.py
@@ -1,19 +1,18 @@
 # pylint: disable=no-member
 
 """Tests for the Operation class."""
-from typing import Tuple
+from dataclasses import dataclass
 
 import pytest
-import rustworkx as rx
 
 from qililab.circuit import Circuit
-from qililab.circuit.nodes import EntryNode, Node, OperationNode
-from qililab.circuit.operations import R180, Barrier, Measure, Operation, Reset, Rxy, Wait, X
-from qililab.circuit.operations.translatable_to_pulse_operations.cphase import CPhase
+from qililab.circuit.operations import Barrier, Measure, Reset, SquarePulse, Wait, X
+from qililab.circuit.operations.operation import Operation
 from qililab.execution.circuit_transpiler import CircuitTranspiler
 from qililab.platform import Platform
 from qililab.settings.runcard_schema import RuncardSchema
-from qililab.typings.enums import OperationMultiplicity, OperationTimingsCalculationMethod
+from qililab.typings.enums import OperationMultiplicity, OperationName, OperationTimingsCalculationMethod
+from qililab.utils import classproperty
 
 
 @pytest.fixture(name="simple_circuit")
@@ -22,7 +21,7 @@ def fixture_simple_circuit() -> Circuit:
     circuit = Circuit(2)
     circuit.add(0, X())
     circuit.add(0, Wait(t=100))
-    circuit.add(1, X())
+    circuit.add(1, SquarePulse(amplitude=1.0, duration=40, resolution=1.0))
     circuit.add((0, 1), Barrier())
     circuit.add(0, X())
     circuit.add(1, X())
@@ -68,7 +67,7 @@ class TestCircuitTranspiler:
         timings_calculation_method: OperationTimingsCalculationMethod,
         platform: Platform,
     ):
-        """Test num_qubits property"""
+        """Test calculate_timings method"""
         circuit = request.getfixturevalue(circuit_fixture)
         settings = platform.settings
         settings.timings_calculation_method = timings_calculation_method
@@ -84,9 +83,31 @@ class TestCircuitTranspiler:
     def test_translate_to_pulse_operations_method(
         self, request: pytest.FixtureRequest, circuit_fixture: str, platform: Platform
     ):
-        """Test num_qubits property"""
+        """Test translate_to_pulses method"""
         circuit = request.getfixturevalue(circuit_fixture)
         transpiler = CircuitTranspiler(circuit, platform.settings)
         circuit_ir2 = transpiler.translate_to_pulse_operations()
         assert isinstance(circuit_ir2, Circuit)
         assert circuit_ir2.depth == circuit.depth
+
+    def test_calculate_timings_method_raises_error_when_operation_not_supported(self, platform: Platform):
+        """Test num_qubits property"""
+
+        @dataclass
+        class UnkownOperation(Operation):
+            def __post_init__(self):
+                self.parameters = {}
+
+            @classproperty
+            def name(self) -> OperationName:
+                return OperationName.X
+
+            @classproperty
+            def multiplicity(self) -> OperationMultiplicity:
+                return OperationMultiplicity.PARALLEL
+
+        circuit = Circuit(1)
+        circuit.add(0, UnkownOperation())
+        transpiler = CircuitTranspiler(circuit, platform.settings)
+        with pytest.raises(ValueError):
+            transpiler.calculate_timings()
