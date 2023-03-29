86aadc5ec225a590338158bb4831df8c4c79246c
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2023-03-29 11:43:44 +0200

    corrected import for the agilent vna driver

--- src/qililab/instruments/agilent/e5071b_vna.py
+++ src/qililab/instruments/agilent/e5071b_vna.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from qililab.instruments.utils import InstrumentFactory
 from qililab.instruments.vector_network_analyzer import VectorNetworkAnalyzer
 from qililab.typings.enums import InstrumentName
-from qililab.typings.instruments.keysight_e5080b import E5080BDriver
+from qililab.typings.instruments.agilent_e5071b import E5071BDriver
 
 
 @InstrumentFactory.register
@@ -12,7 +12,7 @@ class E5071B(VectorNetworkAnalyzer):
     """Agilent Vector Network Analyzer E5071B"""
 
     name = InstrumentName.AGILENT_E5071B
-    device: E5080BDriver
+    device: E5071BDriver
 
     @dataclass
     class E5071BSettings(VectorNetworkAnalyzer.VectorNetworkAnalyzerSettings):
