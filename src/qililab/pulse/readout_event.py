1b8dea6f567e7952cd067813e096e3bc9d8ff97a
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2023-04-04 11:54:12 +0200

    Remove unnecessary ReadoutEvent.from_dict()

--- src/qililab/pulse/readout_event.py
+++ src/qililab/pulse/readout_event.py
@@ -17,18 +17,3 @@ class ReadoutEvent(PulseEvent):
 
     pulses: List[Pulse]
     event_type: ClassVar[PulseEventName] = PulseEventName.READOUT
-
-    @classmethod
-    def from_dict(cls, dictionary: dict):
-        """Load PulseEvent object from dictionary.
-
-        Args:
-            dictionary (dict): Dictionary representation of the PulseEvent object.
-
-        Returns:
-            PulseEvent: Loaded class.
-        """
-        pulses_list = dictionary[PULSEEVENT.PULSES]
-        pulses = [Pulse.from_dict(pulse_dict) for pulse_dict in pulses_list]
-        start_time = dictionary[PULSEEVENT.START_TIME]
-        return cls(pulses=pulses, start_time=start_time)
