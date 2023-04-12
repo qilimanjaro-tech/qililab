83f152a52b8b6df6d59ab9f2816b27819e363241
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2023-04-12 11:53:59 +0200

    Add frequency optional parameter to modulated_waveforms

--- src/qililab/pulse/pulse_event.py
+++ src/qililab/pulse/pulse_event.py
@@ -27,16 +27,17 @@ class PulseEvent:
     def frequency(self) -> float:
         return self.pulse.frequency
 
-    def modulated_waveforms(self, resolution: float = 1.0) -> Waveforms:
+    def modulated_waveforms(self, resolution: float = 1.0, frequency: float = 0.0) -> Waveforms:
         """Applies digital quadrature amplitude modulation (QAM) to the pulse envelope.
 
         Args:
             resolution (float, optional): The resolution of the pulse in ns. Defaults to 1.0.
+            frequency (float, optional): The modulation frequency in Hz, if it is 0.0 then the frequency of the pulse is used. Defaults to 0.0.
 
         Returns:
             Waveforms: I and Q modulated waveforms.
         """
-        return self.pulse.modulated_waveforms(resolution=resolution, start_time=self.start_time)
+        return self.pulse.modulated_waveforms(resolution=resolution, start_time=self.start_time, frequency=frequency)
 
     def to_dict(self):
         """Return dictionary of pulse.
