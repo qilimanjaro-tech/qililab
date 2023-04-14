1be5314b3b8c2e629aa6b0c82f2a2887628bff2b
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-04-14 15:33:32 +0200

    feat: :sparkles: (portfolio): Add T1 experiment.

--- src/qililab/experiment/portfolio/t1.py
+++ src/qililab/experiment/portfolio/t1.py
@@ -1,11 +1,10 @@
 """This file contains a pre-defined version of a T1 experiment."""
-import numpy as np
-from qibo.gates import RX, M, X
+from qibo.gates import M, X
 from qibo.models import Circuit
 
 from qililab.platform import Platform
 from qililab.typings import ExperimentOptions, ExperimentSettings, LoopOptions, Parameter
-from qililab.utils import Loop
+from qililab.utils import Loop, Wait
 
 from .experiment_analysis import ExperimentAnalysis
 from .fitting_models import Exp
@@ -14,14 +13,12 @@ from .fitting_models import Exp
 class T1(ExperimentAnalysis, Exp):
     """Class used to run a T1 experiment on the given qubit.
 
-    This experiment creates multiple circuits, each of which uses an RX(pi/2) gate to send the qubit to the equator of
-    the Bloch sphere, and then applies N R(2pi) gates to flip the qubit around the x axis N times. Given that the
-    amplitude of the X gate is not well calibrated, the qubit will move towards the ground or the excited state.
+    This experiment uses an X gate to excite the qubit, and then waits for a given time before measuring the qubit.
 
     Args:
         platform (Platform): platform used to run the experiment
         qubit (int): qubit index used in the experiment
-        loop_options (LoopOptions): options of the loop used in the experiment, which determines the number of flips
+        loop_options (LoopOptions): wait time loop options
         repetition_duration (int, optional): duration of a single repetition in nanoseconds. Defaults to 10000.
         hardware_average (int, optional): number of repetitions used to average the result. Defaults to 10000.
     """
@@ -34,22 +31,18 @@ class T1(ExperimentAnalysis, Exp):
         repetition_duration=10000,
         hardware_average=10000,
     ):
-        # Define circuits used in this experiment
-        loop = Loop(alias="N", parameter=Parameter.NUM_FLIPS, options=loop_options)
-        circuits = []
-        for n in loop.range:
-            circuit = Circuit(1)
-            circuit.add(RX(qubit, theta=np.pi / 2))
-            for _ in range(n):
-                circuit.add(X(qubit))
-                circuit.add(X(qubit))
-            circuit.add(M(qubit))
-            circuits.append(circuit)
+        # Define circuit used in this experiment
+        circuit = Circuit(qubit)
+        circuit.add(X(qubit))
+        circuit.add(Wait(qubit, t=0))
+        circuit.add(M(qubit))
 
         control_bus, readout_bus = platform.get_bus_by_qubit_index(qubit)
 
+        wait_loop = Loop(alias="0", parameter=Parameter.GATE_PARAMETER, options=loop_options)
         experiment_options = ExperimentOptions(
             name="T1",
+            loops=[wait_loop],
             settings=ExperimentSettings(repetition_duration=repetition_duration, hardware_average=hardware_average),
             plot_y_label="|S21| [dB]",
         )
@@ -57,9 +50,8 @@ class T1(ExperimentAnalysis, Exp):
         # Initialize experiment
         super().__init__(
             platform=platform,
-            circuits=circuits,
+            circuits=[circuit],
             options=experiment_options,
             control_bus=control_bus,
             readout_bus=readout_bus,
-            experiment_loop=loop,
         )
