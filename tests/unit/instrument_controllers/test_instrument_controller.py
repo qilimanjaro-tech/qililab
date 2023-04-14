fe54103593adc178468e4d294a7250ced952415d
Oscar Collado <oscar.collado@qilimanjaro.tech>
2023-04-14 15:34:33 +0200

    test: added test correct number of modules

--- tests/unit/instrument_controllers/test_instrument_controller.py
+++ tests/unit/instrument_controllers/test_instrument_controller.py
@@ -14,10 +14,18 @@ class MiController(SingleInstrumentController):
 
 class TestConnection:
     """This class contains the unit tests for the ``InstrumentController`` class."""
-
+    
     def test_error_raises_when_no_modules(self):
         with pytest.raises(ValueError) as e:
             test = MiController(settings = {'id_':'test', 'category':'test', 'connection':'test', 'modules':'test', 'subcategory':'test'}, loaded_instruments = [])
             test.name = "test"
             test.modules = []
             assert str(e.value) == "The test Instrument Controller requires at least ONE module."
+        
+    def test_error_raises_when_no_compatible_number_modules(self):
+        with pytest.raises(ValueError) as e:
+            test = MiController(settings = {'id_':'test', 'category':'test', 'connection':'test', 'modules':'test', 'subcategory':'test'}, loaded_instruments = ['test'])
+            test.name = "test"
+            test.modules = ['1', '2']
+            assert str(e.value) == "The test Instrument Controller only supports 1 module/s."
+            + "You have connected 2 modules."
