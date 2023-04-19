dcbfb3f66271aed3a908d826ae9ca70db127cbfb
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-04-19 15:09:34 +0200

    Update bias_tee_correction.py

--- src/qililab/pulse/pulse_shape/bias_tee_correction.py
+++ src/qililab/pulse/pulse_shape/bias_tee_correction.py
@@ -12,7 +12,7 @@ from qililab.utils import Factory
 
 
 @Factory.register
-@dataclass(unsafe_hash=True, eq=True)
+@dataclass(unsafe_hash=True, eq=True, frozen = True)
 class BiasTeeCorrection(PulseShape):
     """Bias tee correction pulse shape."""
 
