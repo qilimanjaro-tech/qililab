7d5b668314f32cd0b55e265089a56e12b6f0dbd9
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-04-20 18:31:19 +0200

    update imports to less files

--- src/qililab/pulse/pulse_shape/__init__.py
+++ src/qililab/pulse/pulse_shape/__init__.py
@@ -5,8 +5,4 @@ from .pulse_shape import PulseShape
 from .rectangular import Rectangular
 from .bias_tee_correction import BiasTeeCorrection
 from .exponential_decay_correction import ExponentialCorrection
-from .exponential_decay_correction_v2 import ExponentialCorrectionV2
-from .exponential_decay_correction_v3 import ExponentialCorrectionV3
-from .exponential_decay_correction_loop import ExponentialCorrectionLoop
-from .exponential_decay_correction_loop_v2 import ExponentialCorrectionLoopV2
 from .predistortion import Predistortion
