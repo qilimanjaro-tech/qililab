5da002e49a4bfc6cb1d140abe5ae86431d567e58
Vyron Vasileiadis <hi@fedonman.com>
2023-04-24 11:46:56 +0200

    Feat: Implementation of internal circuit representation (#166)
    * adds initial implementation of qilalb's internal circuit
    
    * remove unnecessary usings
    
    * reverse phi/theta parameters in Rxy operations, add alias to operation node
    
    * rename start_node to entry_node
    
    * rename start_node to entry_node
    
    * convert circuit to qasm, parse from qasm
    
    * edit operations
    
    * implements calculate_timings() of CircuitTranspiler class, refactors operations structure
    
    * implements correct timings calculations for as_late_as_possible method
    
    * test: :test_tube: add tests for Operation and Circuit
    
    * implement OperationFactory class and __str__()/parse() methods of Operation
    
    * updates QiliQasmConverter to use Operation's __str__() & parse() methods
    
    * adds tests for QiliQasmConverter, inits implementation of QiboConverter
    
    * inits tests for QiboConverter
    
    * imlements circuit_transpiler.translate_to_pulse_operations()
    
    * convert operations' name and multiplicity to class properties
    
    * examples
    
    * updates to latest version
    
    * update tests
    
    * update tests
    
    * opt-in mypy ignore errors
    
    * improve tests
    
    * updates tests
    
    * adds transpilation support for Reset operation, updates tests
    
    * fixes typo
    
    * updates tests
    
    * fixes styling
    
    * adds docstrings to Operation, adds signature caching
    
    * adds TranspilerOutputMethod parameter in CircuitTranspiler methods, fixes delay bug
    
    * updates tests
    
    * updates tests
    
    * adds reset_method property to PlatformSettings
    
    * edits Circuit class based on PR comments
    
    * edits Circuit class based on PR comments
    
    * edits operation classes based on PR comments
    
    * removes qibo_converter class
    
    * adds docstrings to qili_qasm_converter.py
    
    * updates transpiler based on PR comments
    
    * updates transpiler based on PR comments
    
    * updates transpiler based on PR comments, adds docstrings to OperationFactory
    
    * fixes ellipsis typo
    
    * refactors circuit_transpiler.calculate_timings() method
    
    * refactors circuit_transpiler.calculate_timings() method
    
    * updates code based on PR comments
    
    * fixes typo in docstring
    
    * removes Pillow and pydot from requirements.txt
    
    * fixes fixture
    
    * removes TranspilerOutputMethod, now tranpiler always deepcopies circuit
    
    * edits example
    
    * edits tests for circuit
    
    * adds tests, edits changelog
    
    * added pydot and Pillow to dev dependencies because tests are failing
    
    * update test.yml configuration file of Actions to install Graphviz
    
    * updates CircuitTranspiler to remove special operations and replace operations with pulses
    
    * adds missing docstrings
    
    * adds missing docstrings and removes unused imports
    
    * removes OperationMultiplicity and adds num_qubits attribute to operations
    
    * edits PulseOperations to respect inheritance correctly
    
    * adds entries to changelog
    
    * reset flags when adding a new operation to the circuit
    
    * adds missing docstring
    
    * fixes typo in runcard
    
    * adds missing imports to circuit module
    
    * updates changelog
    
    ---------
    
    Co-authored-by: Albert Mitjans <a.mitjanscoma@gmail.com>

---
+++ src/qililab/circuit/nodes/__init__.py
@@ -0,0 +1,4 @@
+"""__init__.py"""
+from .entry_node import EntryNode
+from .node import Node
+from .operation_node import OperationNode
---
+++ src/qililab/circuit/operations/translatable_to_pulse_operations/__init__.py
@@ -0,0 +1,5 @@
+from .cphase import CPhase
+from .measure import Measure
+from .parking import Parking
+from .rxy import R180, Rxy, X
+from .translatable_to_pulse_operation import TranslatableToPulseOperation
