75275bf5da11faf960f5b0263451d717fdef2c22
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-04-25 11:28:25 +0200

    style: :art: Delete dead code.

--- src/qililab/utils/util_loops.py
+++ src/qililab/utils/util_loops.py
@@ -6,38 +6,6 @@ import numpy as np
 from qililab.utils.loop import Loop
 
 
-def find_minimum_outer_range_from_loops(loops: List[Loop] | None):
-    """find minimum outer range from same level loops"""
-    if loops is None or len(loops) <= 0:
-        return np.array([], dtype=object)
-    minimum_outer_range = loops[0].outer_loop_range
-    minimum_range_length = len(loops[0].outer_loop_range)
-    for loop in loops:
-        if len(loop.outer_loop_range) < minimum_range_length:
-            minimum_outer_range = loop.outer_loop_range
-            minimum_range_length = len(loop.outer_loop_range)
-    return minimum_outer_range
-
-
-def find_minimum_inner_range_from_loops(loops: List[Loop] | None):
-    """find minimum inner range from same level loops"""
-    if loops is None or len(loops) <= 0:
-        return np.array([], dtype=object)
-
-    if loops[0].inner_loop_range is None:
-        return np.array([], dtype=object)
-    minimum_inner_range = loops[0].inner_loop_range
-    minimum_range_length = len(loops[0].inner_loop_range)
-
-    for loop in loops:
-        if loop.inner_loop_range is None:
-            continue
-        if len(loop.inner_loop_range) < minimum_range_length:
-            minimum_inner_range = loop.inner_loop_range
-            minimum_range_length = len(loop.inner_loop_range)
-    return minimum_inner_range
-
-
 def _find_minimum_range_from_loops(loops: List[Loop] | None):
     """find minimum range from same level loops"""
     if loops is None or len(loops) <= 0:
