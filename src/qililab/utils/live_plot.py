7c015a74a1cff746c7e5cf4e0a573a87e27da89e
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-04-25 11:27:18 +0200

    docs: :pencil: Improve docstrings.

--- src/qililab/utils/live_plot.py
+++ src/qililab/utils/live_plot.py
@@ -12,7 +12,8 @@ from qililab.utils.loop import Loop
 class LivePlot:
     """Class used to live plot experiment results.
 
-    This class supports 1D and 2D plots.
+    This class supports 1D and 2D plots. When running multiple sequences, the sequencer index will always be plotted
+    in the x axis.
 
     Args:
         connection (API): QiboConnection API object.
@@ -37,7 +38,17 @@ class LivePlot:
         self.plot_id = self.create_live_plot(title=title)
 
     def _axis_values(self) -> tuple[list, list | None]:
-        """build plot ranges from defined loop ranges"""
+        """Generate the values for the X and Y axes.
+
+        When plotting more than one sequence, the X axis will correspond to the sequence index. Otherwise, the X axis
+        will correspond to the range of the first (outer) loop.
+
+        If there is only one loop, the Y axis will be None. Otherwise, the Y axis will correspond to the range of the
+        second (inner) loop.
+
+        Returns:
+            tuple[list, list | None]: Values for the X and Y axes respectively.
+        """
         if self.num_schedules > 1:
             x_values = list(range(self.num_schedules))
             return x_values, None if len(self.all_loops) == 0 else list(self.all_loops[0].range)
@@ -59,12 +70,10 @@ class LivePlot:
         return iter(self.x_values), count()
 
     def create_live_plot(self, title: str) -> int:
-        """Create live plot
+        """Create live plot.
 
         Args:
             title (str): Title of the plot.
-            x_label (str): Label of the x axis.
-            y_label (str): Label of the y axis.
         """
         return self.connection.create_liveplot(
             title=title,
@@ -77,10 +86,12 @@ class LivePlot:
         )
 
     def send_points(self, value: float):
-        """Send plot points.
+        """Send points to the live plot.
+
+        This function gathers the X and Y coordinates from the iterators generated in the class constructor.
 
         Args:
-            value (float): value to send to the plot
+            value (float): Value to send to the plot.
         """
         if self.y_values is None:
             x_value = next(self.x_iterator)
