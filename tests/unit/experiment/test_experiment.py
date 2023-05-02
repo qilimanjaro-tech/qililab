18a42a4bbb8003a4a1a4cc0664618746580e5683
Oscar Collado <oscar.collado@qilimanjaro.tech>
2023-05-02 17:43:28 +0200

    style: added fixture and deleted conftest
    fixture added to tests is fixture_simulated_platform

--- tests/unit/experiment/test_experiment.py
+++ tests/unit/experiment/test_experiment.py
@@ -22,8 +22,31 @@ from qililab.typings.enums import InstrumentName
 from qililab.typings.experiment import ExperimentOptions
 from qililab.utils import Loop
 from qililab.utils.live_plot import LivePlot
-from tests.data import Galadriel, experiment_params, simulated_experiment_circuit
+from tests.data import FluxQubitSimulator, Galadriel, experiment_params, simulated_experiment_circuit
 from tests.utils import mock_instruments, platform_db
+
+
+@pytest.fixture(name="simulated_platform")
+@patch("qililab.system_control.simulated_system_control.Evolution", autospec=True)
+def fixture_simulated_platform(mock_evolution: MagicMock) -> Platform:
+    """Return Platform object."""
+
+    # Mocked Evolution needs: system.qubit.frequency, psi0, states, times
+    mock_system = MagicMock()
+    mock_system.qubit.frequency = 0
+    mock_evolution.return_value.system = mock_system
+    mock_evolution.return_value.states = []
+    mock_evolution.return_value.times = []
+    mock_evolution.return_value.psi0 = None
+
+    with patch(
+        "qililab.platform.platform_manager_yaml.yaml.safe_load", return_value=FluxQubitSimulator.runcard
+    ) as mock_load:
+        with patch("qililab.platform.platform_manager_yaml.open") as mock_open:
+            platform = build_platform(name="flux_qubit")
+            mock_load.assert_called()
+            mock_open.assert_called()
+    return platform
 
 
 @pytest.fixture(name="nested_experiment", params=experiment_params)
