db6c35f86efab62b2791d3f793373ca8048ec8a2
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2023-05-03 12:56:16 +0200

    Remove probabilities implementation on qblox_result

--- src/qililab/result/qblox_results/qblox_result.py
+++ src/qililab/result/qblox_results/qblox_result.py
@@ -125,14 +125,6 @@ class QbloxResult(Result):
             )
         return acquisitions.scope.path0.data, acquisitions.scope.path1.data
 
-    def probabilities(self) -> dict[str, float]:
-        """Return probabilities of being in the ground and excited state.
-
-        Returns:
-            dict[str, float]: Dictionary containing the probabilities (value) of each state (key).
-        """
-        return self.qblox_bins_acquisitions.probabilities()
-
     def counts(self) -> Counts:
         """Returns a Counts object containing the counts of each state.
 
