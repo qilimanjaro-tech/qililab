1c570296d4de88a78ccaa7fbb5a5e6167dcd11ec
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-05-08 12:37:25 +0200

    Separate into two fixtures

--- tests/unit/pulse/pulse_distortion/test_bias_tee_correction.py
+++ tests/unit/pulse/pulse_distortion/test_bias_tee_correction.py
@@ -13,6 +13,14 @@ from qililab.typings.enums import PulseDistortionSettingsName
 # Parameters for the BiasTeeCorrection.
 TAU_BIAS_TEE = [0.5, 0.9, 1.3]
 
+# Parameters of the Pulse and its envelope.
+AMPLITUDE = [0.9]
+PHASE = [0, np.pi / 3, 2 * np.pi, 3 * np.pi]
+DURATION = [1, 2, 47]  # TODO: Add 0 to this test?
+FREQUENCY = [0.7e9]
+RESOLUTION = [1.1]
+SHAPE = [Rectangular(), Gaussian(num_sigmas=4), Drag(num_sigmas=4, drag_coefficient=1.0)]
+
 
 @pytest.fixture(
     name="distortion",
@@ -23,35 +31,31 @@ def fixture_distortion(request: pytest.FixtureRequest) -> BiasTeeCorrection:
     return request.param
 
 
+@pytest.fixture(
+    name="envelope",
+    params=[
+        Pulse(amplitude=amplitude, phase=phase, duration=duration, frequency=frequency, pulse_shape=shape).envelope(
+            resolution=resolution
+        )
+        for amplitude, phase, duration, frequency, resolution, shape in itertools.product(
+            AMPLITUDE, PHASE, DURATION, FREQUENCY, RESOLUTION, SHAPE
+        )
+    ],
+)
+def fixture_envelope(request: pytest.FixtureRequest) -> np.ndarray:
+    """Fixture for the pulse distortion class."""
+    return request.param
+
+
 class TestBiasTeeCorrection:
     """Unit tests checking the BiasTeeCorrection attributes and methods"""
 
-    def test_apply(self, distortion: BiasTeeCorrection):
+    def test_apply(self, distortion: BiasTeeCorrection, envelope: np.ndarray):
         """Test for the envelope method."""
-        # Parameters of the Pulse and its envelope.
-        AMPLITUDE = [0.9]
-        PHASE = [0, np.pi / 3, 2 * np.pi, 3 * np.pi]
-        DURATION = [1, 2, 47]  # TODO: Add 0 to this test?
-        FREQUENCY = [0.7e9]
-        RESOLUTION = [1.1]
-        SHAPE = [Rectangular(), Gaussian(num_sigmas=4), Drag(num_sigmas=4, drag_coefficient=1.0)]
-
-        envelopes = []
-        corr_envelopes = []
-
-        for amplitude, phase, duration, frequency, resolution, shape in itertools.product(
-            AMPLITUDE, PHASE, DURATION, FREQUENCY, RESOLUTION, SHAPE
-        ):
-            envelope = Pulse(
-                amplitude=amplitude, phase=phase, duration=duration, frequency=frequency, pulse_shape=shape
-            ).envelope(resolution=resolution)
-            envelopes.append(envelope)
-
-        for counter, envelope in enumerate(envelopes):
-            corr_envelopes.append(distortion.apply(envelope=envelope))
-            corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=1.3).apply(envelope=corr_envelopes[0 + counter * 4]))
-            corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1 + counter * 4]))
-            corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1 + counter * 4]))
+        corr_envelopes = [distortion.apply(envelope=envelope)]
+        corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=1.3).apply(envelope=corr_envelopes[0]))
+        corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1]))
+        corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1]))
 
         for corr_envelope in corr_envelopes:
             assert corr_envelope is not None
--- tests/unit/pulse/pulse_distortion/test_pulse_distortion.py
+++ tests/unit/pulse/pulse_distortion/test_pulse_distortion.py
@@ -16,6 +16,14 @@ TAU_BIAS_TEE = [0.5, 0.9, 1.3]
 TAU_EXPONENTIAL = [0.5, 0.9, 1.3]
 AMP = [-5.1, 1.2, 0.8, 2.0]
 
+# Parameters of the Pulse and its envelope.
+AMPLITUDE = [0.9]
+PHASE = [0, np.pi / 3, 2 * np.pi, 3 * np.pi]
+DURATION = [1, 2, 47]  # TODO: Add 0 to this test?
+FREQUENCY = [0.7e9]
+RESOLUTION = [1.1]
+SHAPE = [Rectangular(), Gaussian(num_sigmas=4), Drag(num_sigmas=4, drag_coefficient=1.0)]
+
 
 @pytest.fixture(
     name="distortion",
@@ -30,39 +38,31 @@ def fixture_distortion(request: pytest.FixtureRequest) -> ExponentialCorrection:
     return request.param
 
 
+@pytest.fixture(
+    name="envelope",
+    params=[
+        Pulse(amplitude=amplitude, phase=phase, duration=duration, frequency=frequency, pulse_shape=shape).envelope(
+            resolution=resolution
+        )
+        for amplitude, phase, duration, frequency, resolution, shape in itertools.product(
+            AMPLITUDE, PHASE, DURATION, FREQUENCY, RESOLUTION, SHAPE
+        )
+    ],
+)
+def fixture_envelope(request: pytest.FixtureRequest) -> np.ndarray:
+    """Fixture for the pulse distortion class."""
+    return request.param
+
+
 class TestPulseDistortion:
     """Unit tests checking the PulseDistortion attributes and methods"""
 
-    def test_apply(self, distortion: PulseDistortion):
+    def test_apply(self, distortion: PulseDistortion, envelope: np.ndarray):
         """Test for the apply method."""
-        # Parameters of the Pulse and its envelope.
-        AMPLITUDE = [0.9]
-        PHASE = [0, np.pi / 3, 2 * np.pi, 3 * np.pi]
-        DURATION = [1, 2, 47]  # TODO: Add 0 to this test?
-        FREQUENCY = [0.7e9]
-        RESOLUTION = [1.1]
-        SHAPE = [Rectangular(), Gaussian(num_sigmas=4), Drag(num_sigmas=4, drag_coefficient=1.0)]
-
-        envelopes = []
-        corr_envelopes = []
-
-        for amplitude, phase, duration, frequency, resolution, shape in itertools.product(
-            AMPLITUDE, PHASE, DURATION, FREQUENCY, RESOLUTION, SHAPE
-        ):
-            envelope = Pulse(
-                amplitude=amplitude, phase=phase, duration=duration, frequency=frequency, pulse_shape=shape
-            ).envelope(resolution=resolution)
-            envelopes.append(envelope)
-
-        for counter, envelope in enumerate(envelopes):
-            corr_envelopes.append(distortion.apply(envelope=envelope))
-            corr_envelopes.append(
-                ExponentialCorrection(tau_exponential=1.3, amp=2.0).apply(envelope=corr_envelopes[0 + counter * 4])
-            )
-            corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1 + counter * 4]))
-            corr_envelopes.append(
-                ExponentialCorrection(tau_exponential=0.5, amp=-5.0).apply(envelope=corr_envelopes[1 + counter * 4])
-            )
+        corr_envelopes = [distortion.apply(envelope=envelope)]
+        corr_envelopes.append(ExponentialCorrection(tau_exponential=1.3, amp=2.0).apply(envelope=corr_envelopes[0]))
+        corr_envelopes.append(BiasTeeCorrection(tau_bias_tee=0.5).apply(envelope=corr_envelopes[1]))
+        corr_envelopes.append(ExponentialCorrection(tau_exponential=0.5, amp=-5.0).apply(envelope=corr_envelopes[1]))
 
         for corr_envelope in corr_envelopes:
             assert corr_envelope is not None
