9c8feac0493293ded79b8b3aae5056841de15979
Albert Mitjans <a.mitjanscoma@gmail.com>
2023-05-12 10:57:53 +0200

    Add QRM-RF module. (#330)
    * feat: :sparkles: Add QCM-RF module.
    
    * fix: :bug: Small fix.
    
    * test: :test_tube: Add test.
    
    * test: :test_tube: Add tests.
    
    * docs: :pencil: Add changelog entry.
    
    * test: :test_tube: Fix tests.
    
    * test: :test_tube: Fix tests.
    
    * test: :test_tube: Add tests.
    
    * feat: :sparkles: Add QRM-RF module.
    
    * revert: :rewind: Revert changes.
    
    * test: :test_tube: Fix test.
    
    * refactor: :recycle: Remove dead code.
    
    * test: :test_tube: Remove unused import.
    
    * test: :test_tube: Add tests.
    
    * refactor: :recycle: Small refactor.
    
    * test: :test_tube: Fix test.
    
    * docs: :pencil: Add changelog entry.
    
    * ci: :construction_worker: Remove dummyqblox from requirements.txt
    
    * test: :test_tube: Fix tests.
    
    * style: :art: Use Enum.
    
    * test: :test_tube: Separate tests.
    
    * test: :test_tube: Use pytest.approx in test.
    
    * style: :art: Use Enum.
    
    * test: :test_tube: Remove comment.
    
    * test: :test_tube: Separate tests.
    
    * test: :test_tube: Add comment.
    
    * fix: :bug: Add import.
    
    * fix: :bug: Small fix.

--- src/qililab/instruments/qblox/__init__.py
+++ src/qililab/instruments/qblox/__init__.py
@@ -5,4 +5,5 @@ from .qblox_module import QbloxModule
 from .qblox_qcm import QbloxQCM
 from .qblox_qcm_rf import QbloxQCMRF
 from .qblox_qrm import QbloxQRM
+from .qblox_qrm_rf import QbloxQRMRF
 from .qblox_s4g import QbloxS4g
