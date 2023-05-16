72bb485ecb669eab80a3e654f8a2058d931cbfa6
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2023-05-16 15:14:43 +0200

    codecov increase

--- tests/unit/experiment/test_exp.py
+++ tests/unit/experiment/test_exp.py
@@ -125,12 +125,18 @@ def fixture_exp(request: pytest.FixtureRequest):
             platform = build_platform(name="galadriel")
             mock_load.assert_called()
             mock_open.assert_called()
-    loop = Loop(
+    loop_1 = Loop(
         alias=Galadriel.buses[0][RUNCARD.ALIAS],
         parameter=Parameter.DURATION,
         values=np.arange(start=4, stop=1000, step=40),
     )
-    options = ExperimentOptions(loops=[loop])
+    # Adds more coverage to tests
+    loop_2 = Loop(
+        alias=Galadriel.buses[0][RUNCARD.ALIAS],
+        parameter=Parameter.EXTERNAL,
+        values=np.arange(start=4, stop=1000, step=40),
+    )
+    options = ExperimentOptions(loops=[loop_1, loop_2])
     return Exp(platform=platform, options=options)
 
 
@@ -293,6 +299,15 @@ class TestMethods:
         expected = f"Experiment {experiment_all_platforms.options.name}:\n{str(experiment_all_platforms.platform)}\n{str(experiment_all_platforms.options)}"
         test_str = str(experiment_all_platforms)
         assert expected == test_str
+
+    def test_filter_loops_values_with_external_parameters_raises_exception(self, exp: Exp):
+        """Test _filter_loops_values_with_external_paramters raises an exception when lists of values and loops have not the same length"""
+        values = (1.5, -1.5)
+        loops = [Loop(alias="foo", parameter=Parameter.POWER, values=np.linspace(0, 10, 10))]
+        with pytest.raises(
+            ValueError, match=f"Values list length: {len(values)} differ from loops list length: {len(loops)}."
+        ):
+            exp._filter_loops_values_with_external_parameters(values, loops)
 
 
 class TestSetParameter:
