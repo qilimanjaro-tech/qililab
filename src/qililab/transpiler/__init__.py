19e01f298ea7f446767696650819d40bc12d96ad
Victor <visangim@gmail.com>
2023-05-23 17:07:33 +0200

    added init entries

--- src/qililab/transpiler/__init__.py
+++ src/qililab/transpiler/__init__.py
@@ -1,5 +1,6 @@
 """__init__.py"""
 
+from .execute_qibo_circuit import execute_qibo_circuit
 from .gate_decompositions import translate_gates
 from .native_gates import Drag
 from .transpiler import translate_circuit
