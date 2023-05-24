e61377451ef6050ac04d73f2aee82a67f153ffea
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2023-05-24 12:54:38 +0200

    fix: loop values types

--- src/qililab/utils/loop.py
+++ src/qililab/utils/loop.py
@@ -52,7 +52,10 @@ class Loop:
             self.loop.previous = self
         if isinstance(self.parameter, str):
             self.parameter = Parameter(self.parameter)
-        if isinstance(self.values, list):
+        if isinstance(self.values, np.ndarray):
+            new_values = self.values.tolist()  # Convert type of elements `np.int64` to `int`
+            self.values = np.array(new_values, dtype=object)
+        elif isinstance(self.values, list):
             self.values = np.array(self.values, dtype=object)
 
     @property
