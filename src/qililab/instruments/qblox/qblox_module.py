ad6d8ebf07e44898468cef10b638c3ed9416088b
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2023-05-26 18:08:32 +0200

    Save acquisitions inside binning loop

--- src/qililab/instruments/qblox/qblox_module.py
+++ src/qililab/instruments/qblox/qblox_module.py
@@ -198,26 +198,26 @@ class QbloxModule(AWG):
         # Define program's blocks
         program = Program()
         avg_loop = Loop(name="average", begin=int(self.nshots))  # type: ignore
-        # bin_loop = Loop(name="bin", begin=0, end=self.num_bins, step=1)
-        # avg_loop.append_component(bin_loop)
+        bin_loop = Loop(name="bin", begin=0, end=self.num_bins, step=1)
+        avg_loop.append_component(bin_loop)
         program.append_block(avg_loop)
         stop = Block(name="stop")
         stop.append_component(Stop())
         program.append_block(block=stop)
         timeline = pulse_bus_schedule.timeline
         if len(timeline) > 0 and timeline[0].start_time != 0:
-            avg_loop.append_component(Wait(wait_time=int(timeline[0].start_time)))
+            bin_loop.append_component(Wait(wait_time=int(timeline[0].start_time)))
 
         for i, pulse_event in enumerate(timeline):
             waveform_pair = waveforms.find_pair_by_name(pulse_event.pulse.label())
             wait_time = timeline[i + 1].start_time - pulse_event.start_time if (i < (len(timeline) - 1)) else 4
-            avg_loop.append_component(ResetPh())
+            bin_loop.append_component(ResetPh())
             gain = int(np.abs(pulse_event.pulse.amplitude) * AWG_MAX_GAIN)  # np.abs() needed for negative pulses
-            avg_loop.append_component(SetAwgGain(gain_0=gain, gain_1=gain))
+            bin_loop.append_component(SetAwgGain(gain_0=gain, gain_1=gain))
             phase = int((pulse_event.pulse.phase % 360) * 1e9 / 360)
-            avg_loop.append_component(SetPh(phase=phase))
+            bin_loop.append_component(SetPh(phase=phase))
             # avg_loop.append_component(SetMrk(marker_outputs=15))
-            avg_loop.append_component(
+            bin_loop.append_component(
                 Play(
                     waveform_0=waveform_pair.waveform_i.index,
                     waveform_1=waveform_pair.waveform_q.index,
@@ -226,11 +226,11 @@ class QbloxModule(AWG):
             )
             # avg_loop.append_component(SetMrk(marker_outputs=0))
             # avg_loop.append_component(UpdParam(wait_time=4))
-        self._append_acquire_instruction(loop=avg_loop, bin_index=0, sequencer_id=sequencer)
+        self._append_acquire_instruction(loop=bin_loop, bin_index=bin_loop.counter_register, sequencer_id=sequencer)
         if self.repetition_duration is not None:
-            wait_time = self.repetition_duration - avg_loop.duration_iter
+            wait_time = self.repetition_duration - bin_loop.duration_iter
             if wait_time > self._MIN_WAIT_TIME:
-                avg_loop.append_component(long_wait(wait_time=wait_time))
+                bin_loop.append_component(long_wait(wait_time=wait_time))
 
         logger.info("Q1ASM program: \n %s", repr(program))  # pylint: disable=protected-access
         return program
