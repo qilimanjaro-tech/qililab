e5cae697e34ddfcb55c82e7ee92729a35ac4e169
Vyron Vasileiadis <hi@fedonman.com>
2023-06-07 20:33:44 +0200

    Add T1 portfolio experiment (#409)
    * Add T1 portfolio experiment
    
    * Changelog: Add entries
    
    * T1: Refactor tests
    
    * T1: Refactor tests

--- tests/unit/experiment/portfolio/test_fitting_models.py
+++ tests/unit/experiment/portfolio/test_fitting_models.py
@@ -1,7 +1,7 @@
 """Unit tests for the fitting models."""
 import numpy as np
 
-from qililab.experiment.portfolio import CosFunc
+from qililab.experiment.portfolio import Cos, Exp
 
 
 class TestFittingModels:
@@ -14,5 +14,14 @@ class TestFittingModels:
         frequency = 1.23
         phase = 3.45
         offset = 5.67
-        ydata = CosFunc.func(xdata, amplitude, frequency, phase, offset)
+        ydata = Cos.func(xdata, amplitude, frequency, phase, offset)
         assert np.allclose(ydata, amplitude * np.cos(2 * np.pi * frequency * xdata + phase) + offset)
+
+    def test_exp_func(self):
+        """Test the exponential function."""
+        xdata = np.linspace(0, 100)
+        amplitude = 10
+        rate = 0.5
+        offset = 5.67
+        ydata = Exp.func(xdata, amplitude, rate, offset)
+        assert np.allclose(ydata, amplitude * np.exp(rate * xdata) + offset)
