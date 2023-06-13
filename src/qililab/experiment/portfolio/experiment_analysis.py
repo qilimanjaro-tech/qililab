c2a847f931a10f354c95ab39b2fe16b13e089758
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-06-13 10:09:55 +0200

    provisional changes

--- src/qililab/experiment/portfolio/experiment_analysis.py
+++ src/qililab/experiment/portfolio/experiment_analysis.py
@@ -41,7 +41,7 @@ class ExperimentAnalysis(Experiment, FittingModel):
         platform: Platform,
         circuits: list[Circuit],
         options: ExperimentOptions,
-        control_bus: Bus | None = None,  # TODO: This will probably change for 2-qubit experiments
+        control_bus: Bus | None = None,
         readout_bus: Bus | None = None,
         experiment_loop: Loop | None = None,
     ):
@@ -51,18 +51,23 @@ class ExperimentAnalysis(Experiment, FittingModel):
                     "A loop must be provided. Either an experiment loop in the `ExperimentOptions` class, or "
                     "an external loop in the `experiment_loop` argument."
                 )
-            self.loop = options.loops[0]  # TODO: Support nested loops
+            self.loops = options.loops
         else:
-            self.loop = experiment_loop
+            self.loops = [experiment_loop]
+
         self.control_bus = control_bus
         self.readout_bus = readout_bus
+        self.two_dimension_experiment = False
+        if len(self.loops) == 2:
+            self.two_dimension_experiment = True
+
         super().__init__(platform=platform, circuits=circuits, options=options)
 
     def post_process_results(self):
         """Method used to post-process the results of an experiment.
 
         By default this method computes the magnitude of the IQ data and saves it into the ``post_processed_results``
-        attribute.
+        attribute. If the experiment is a 2D one, it reshapes the data accordingly.
 
         Returns:
             np.ndarray: post-processed results
@@ -71,6 +76,12 @@ class ExperimentAnalysis(Experiment, FittingModel):
         i = np.array(acquisitions["i"])
         q = np.array(acquisitions["q"])
         self.post_processed_results = 20 * np.log10(np.sqrt(i**2 + q**2))
+        
+        if self.two_dimension_experiment:
+            self.post_processed_results = self.post_processed_results.reshape(
+                len(self.loops[0].values), len(self.loops[1].values)
+            )
+
         return self.post_processed_results
 
     def fit(self, p0: tuple | None = None):
@@ -82,7 +93,7 @@ class ExperimentAnalysis(Experiment, FittingModel):
             p0 (tuple, optional): Initial guess for the parameters. Defaults to None.
 
         Returns:
-            float: optimal values for the parameters so that the sum of the squared residuals of
+            list(float): list with optimal values for the parameters so that the sum of the squared residuals of
                 ``f(xdata, *popt) - ydata is minimized.
         """
         if not hasattr(self, "post_processed_results"):
@@ -91,37 +102,92 @@ class ExperimentAnalysis(Experiment, FittingModel):
                 "Please call ``post_process_results`` first."
             )
 
-        self.popt, _ = curve_fit(  # pylint: disable=unbalanced-tuple-unpacking
-            self.func, xdata=self.loop.values, ydata=self.post_processed_results, p0=p0
-        )
+        self.popts =[curve_fit(  # pylint: disable=unbalanced-tuple-unpacking
+            self.func, xdata=loop.values, ydata=self.post_processed_results, p0=p0
+        ) for loop in self.loops]
 
-        return self.popt
+        return self.popts
 
-    def plot(self):
-        """Method used to plot the results of an experiment.
-
+    def plot_1D(self, y_label:str=""):
+        """Method used to generate default 1D plot.
+        
         By default this method creates a figure with size (9, 7) and plots the magnitude of the IQ data.
+        
+        Args:
+            y_label (str, optional): string indicating the y_label for 1D plot.
+        
+        Returns:
+            matplotlib.figure: matplotlib figure with 1D plot.
         """
         if not hasattr(self, "post_processed_results"):
             raise AttributeError(
                 "The post-processed results must be computed before fitting. "
                 "Please call ``post_process_results`` first."
             )
-        xdata = self.loop.values
+        loop = self.loops[0]
+        xdata = loop.values
 
         # Plot data
         fig, axes = plt.subplots(figsize=(9, 7))
         axes.set_title(self.options.name)
-        axes.set_xlabel(f"{self.loop.alias}: {self.loop.parameter.value}")
-        axes.set_ylabel("|S21| [dB]")  # TODO: Change label for 2D plots
+        axes.set_xlabel(f"{loop.alias}: {loop.parameter.value}")
+        axes.set_ylabel(y_label)
         axes.scatter(xdata, self.post_processed_results, color="blue")
-        if hasattr(self, "popt"):
+        if hasattr(self, "popts"):
             # Create label text
+            popt = self.popts[0]
             args = list(inspect.signature(self.func).parameters.keys())[1:]
-            text = "".join(f"{arg}: {value:.5f}\n" for arg, value in zip(args, self.popt))
-            axes.plot(xdata, self.func(xdata, *self.popt), "--", label=text, color="red")
+            print(popt[0])
+            print(type(popt[0]))
+            text = "".join(f"{arg}:{value}\n" for arg, value in zip(args, popt))
+            print("TEXT: ", text)
+            """print("TEXTO: ", text)
+            for arg, value in zip(args, self.popts[0]):
+                print(f"{arg}")
+                print("string for label: ", f"{arg}: {value:.5f}\n")
+            text = "".join(f"{arg}: {value:.5f}\n" for arg, value in zip(args, self.popts[0]))"""
+            axes.plot(xdata, self.func(xdata, *popt), "--", label=text, color="red")
             axes.legend(loc="upper right")
+
         return fig
+
+    def plot_2D(self, x_label:str="", y_label:str=""):
+        """Method used to generate default 2D plots.
+        
+        Args:
+            x_label (str, optional): string indicating the x_label for the 2D plot.
+            y_label (str, optional): string indicating the y_label for the 2D plot.
+        
+        Returns:
+            matplotlib.figure: matplotlib figure with 2D plot.
+        """
+        fig = plt.figure()
+        for ii, _ in enumerate(self.loops[0].values):
+            plt.plot(self.loops[1].values, self.post_processed_results[ii, :],
+                        '-o', label=f"{self.loops[1].alias}: {self.loops[1].parameter.value}")
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.legend()
+        
+        return fig
+        
+
+    def plot(self, x_label:str="", y_label:str=""):
+        """Method used to generate default plots. It checks if the experiment is 1D or 2D and calls the 
+        appropiate function.
+        
+        Args:
+            x_label (str, optional): string indicating the x_label for 2D plots
+            y_label (str, optional): string indicating the y_label for 1D and 2D plots
+        
+        Returns:
+            matplotlib.figure: matplotlib figure with either 1D plot or 2D plots.
+        """
+        if self.two_dimension_experiment:
+            return self.plot_2D(x_label=x_label,
+                                y_label=y_label)
+        else:
+            return self.plot_1D(y_label=y_label)
 
     def bus_setup(self, parameters: dict[Parameter, float | str | bool], control=False) -> None:
         """Method used to change parameters of the bus used in the experiment. Some possible bus parameters are:
--- tests/unit/experiment/portfolio/test_experiment_analysis.py
+++ tests/unit/experiment/portfolio/test_experiment_analysis.py
@@ -24,8 +24,12 @@ START = 1
 STOP = 5
 NUM = 1000
 x = np.linspace(START, STOP, NUM)
-i = 5 * np.sin(7 * x)
-q = 9 * np.sin(7 * x)
+I_AMPLITUDE, I_RATE = (5, 7)
+Q_AMPLITUDE, Q_RATE = (9, 7)
+i = I_AMPLITUDE * np.sin(I_RATE * x)
+q = Q_AMPLITUDE * np.sin(Q_RATE * x)
+outer_loop_num_values = 50
+inner_loop_num_values = 40
 
 
 class DummyFittingModel(FittingModel):
@@ -38,8 +42,8 @@ class DummyExperimentAnalysis(ExperimentAnalysis, DummyFittingModel):
     """Dummy class used to test the ``ExperimentAnalysis`` class."""
 
 
-@pytest.fixture(name="experiment_analysis")
-def fixture_experiment_analysis():
+@pytest.fixture(name="experiment_analysis_1D")
+def fixture_experiment_analysis_1D():
     """Return Experiment object."""
     with patch("qililab.platform.platform_manager_yaml.yaml.safe_load", return_value=Galadriel.runcard) as mock_load:
         with patch("qililab.platform.platform_manager_yaml.open") as mock_open:
@@ -60,6 +64,32 @@ def fixture_experiment_analysis():
     }
     return analysis
 
+@pytest.fixture(name="experiment_analysis_2D")
+def fixture_experiment_analysis_2D():
+    """Return Experiment object."""
+    with patch("qililab.platform.platform_manager_yaml.yaml.safe_load", return_value=Galadriel.runcard) as mock_load:
+        with patch("qililab.platform.platform_manager_yaml.open") as mock_open:
+            platform = build_platform(name="flux_qubit")
+            mock_load.assert_called()
+            mock_open.assert_called()
+    outer_loop = Loop(
+        alias="X(0)",
+        parameter=Parameter.DURATION,
+        values=x[:outer_loop_num_values],
+    )
+    inner_loop = Loop(
+        alias="Y(0)",
+        parameter=Parameter.DURATION,
+        values=x[:inner_loop_num_values],
+    )
+    options = ExperimentOptions(loops=[outer_loop, inner_loop])
+    analysis = DummyExperimentAnalysis(platform=platform, circuits=[circuit], options=options)
+    analysis.results = MagicMock()
+    analysis.results.acquisitions.return_value = {
+        "i": np.concatenate((i, i)),
+        "q": np.concatenate((q, q))
+    }
+    return analysis
 
 class TestExperimentAnalysis:
     """Unit tests for the ``ExperimentAnalysis`` class."""
@@ -73,67 +103,131 @@ class TestExperimentAnalysis:
         ):
             DummyExperimentAnalysis(platform=MagicMock(), circuits=[circuit], options=ExperimentOptions())
 
-    def test_post_process_results(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_post_process_results(self,
+                                  experiment_analysis_1D: DummyExperimentAnalysis,
+                                  experiment_analysis_2D: DummyExperimentAnalysis):
         """Test post_process_results method."""
-        res = experiment_analysis.post_process_results()
+        res = experiment_analysis_1D.post_process_results()
         assert all(res == 20 * np.log10(np.sqrt(i**2 + q**2)))
+        
+        res = experiment_analysis_2D.post_process_results()
+        assert res.shape == (outer_loop_num_values, inner_loop_num_values)
+        res_outer_loop = res[0]
+        #print("RES 0: ", res[0])
+        assert all(res_outer_loop == 20 * np.log10(np.sqrt(i**2 + q**2)))
+        
 
-    def test_fit(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_fit(self,
+                 experiment_analysis_1D: DummyExperimentAnalysis,
+                 experiment_analysis_2D: DummyExperimentAnalysis):
         """Test fit method."""
-        experiment_analysis.post_processed_results = q
-        popt = experiment_analysis.fit(p0=(8, 7.5))  # p0 is an initial guess
-        assert np.allclose(popt, (9, 7), atol=1e-5)
+        experiment_analysis_1D.post_processed_results = q
+        popts = experiment_analysis_1D.fit(p0=(8, 7.5))[0]  # p0 is an initial guess
+        assert np.allclose(popts, (9, 7), atol=1e-5)
+        
+        #experiment_analysis_2D.post_processed_results = experiment_analysis_2D.post_process_results()
+        #popts = experiment_analysis_2D.fit(p0=(8, 7.5))  # p0 is an initial guess
+        #assert np.allclose(popts[0], (9, 7), atol=1e-5)
+        #assert np.allclose(popts[1], (9, 7), atol=1e-5)
 
-    def test_fit_raises_error_when_no_post_processing(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_fit_raises_error_when_no_post_processing(self,
+                                                      experiment_analysis_1D: DummyExperimentAnalysis,
+                                                      experiment_analysis_2D: DummyExperimentAnalysis):
         """Test that the ``fit`` method raises an error when the results are not post processed."""
         with pytest.raises(AttributeError, match="The post-processed results must be computed before fitting."):
-            experiment_analysis.fit(p0=(8, 7.5))
+            experiment_analysis_1D.fit(p0=(8, 7.5))
+        with pytest.raises(AttributeError, match="The post-processed results must be computed before fitting."):
+            experiment_analysis_2D.fit(p0=(8, 7.5))
 
-    def test_plot(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_plot(self,
+                  experiment_analysis_1D: DummyExperimentAnalysis,
+                  experiment_analysis_2D: DummyExperimentAnalysis):
         """Test plot method."""
-        experiment_analysis.post_processed_results = q
-        popt = experiment_analysis.fit()
-        fig = experiment_analysis.plot()
+        experiment_analysis_1D.post_processed_results = q
+        popts = experiment_analysis_1D.fit()
+        fig = experiment_analysis_1D.plot()
         scatter_data = fig.findobj(match=lambda x: hasattr(x, "get_offsets"))[0].get_offsets()
         assert np.allclose(scatter_data[:, 0], x)
         assert np.allclose(scatter_data[:, 1], q)
         ax = fig.axes[0]
         line = ax.lines[0]
         assert np.allclose(line.get_xdata(), x)
-        assert np.allclose(line.get_ydata(), popt[0] * np.sin(popt[1] * x))
+        assert np.allclose(line.get_ydata(), popts[0][0] * np.sin(popts[0][1] * x))
+        
+        experiment_analysis_2D.post_processed_results = np.concatenate(q, q).reshape(len(q), len(q))
+        popts = experiment_analysis_2D.fit()
+        fig = experiment_analysis_2D.plot()
+        scatter_data = fig.findobj(match=lambda x: hasattr(x, "get_offsets"))[0].get_offsets()
+        assert np.allclose(scatter_data[:, 0], x)
+        assert np.allclose(scatter_data[:, 1], q)
+        ax = fig.axes[0]
+        line = ax.lines[0]
+        assert np.allclose(line.get_xdata(), x)
+        assert np.allclose(line.get_ydata(), popts[0][0] * np.sin(popts[0][1] * x))
 
-    def test_plot_raises_error_when_no_post_processing(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_plot_raises_error_when_no_post_processing(self,
+                                                       experiment_analysis_1D: DummyExperimentAnalysis):
         """Test that the ``plot`` method raises an error when the results are not post processed."""
         with pytest.raises(AttributeError, match="The post-processed results must be computed before fitting."):
-            experiment_analysis.plot()
+            experiment_analysis_1D.plot()
 
-    def test_bus_setup_with_control_true(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_bus_setup_with_control_true(self,
+                                         experiment_analysis_1D: DummyExperimentAnalysis,
+                                         experiment_analysis_2D: DummyExperimentAnalysis):
         """Test the ``bus_setup`` method with ``control=True``."""
-        experiment_analysis.control_bus = MagicMock()
-        experiment_analysis.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=True)
-        experiment_analysis.control_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
+        experiment_analysis_1D.control_bus = MagicMock()
+        experiment_analysis_1D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=True)
+        experiment_analysis_1D.control_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
 
-    def test_bus_setup_with_control_false(self, experiment_analysis: DummyExperimentAnalysis):
+        experiment_analysis_2D.control_bus = MagicMock()
+        experiment_analysis_2D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=True)
+        experiment_analysis_2D.control_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
+        
+    def test_bus_setup_with_control_false(self,
+                                         experiment_analysis_1D: DummyExperimentAnalysis,
+                                         experiment_analysis_2D: DummyExperimentAnalysis):
         """Test the ``bus_setup`` method with ``control=False``."""
-        experiment_analysis.readout_bus = MagicMock()
-        experiment_analysis.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=False)
-        experiment_analysis.readout_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
+        experiment_analysis_1D.readout_bus = MagicMock()
+        experiment_analysis_1D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=False)
+        experiment_analysis_1D.readout_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
+        
+        experiment_analysis_2D.readout_bus = MagicMock()
+        experiment_analysis_2D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6}, control=False)
+        experiment_analysis_2D.readout_bus.set_parameter.assert_called_once_with(parameter=Parameter.AMPLITUDE, value=0.6)
 
-    def test_bus_setup_raises_error_when_no_bus_is_found(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_bus_setup_raises_error_when_no_bus_is_found(self,
+                                                         experiment_analysis_1D: DummyExperimentAnalysis,
+                                                         experiment_analysis_2D: DummyExperimentAnalysis):
         """Test that the ``bus_setup`` method raises an error when no bus is found."""
         with pytest.raises(ValueError, match="The experiment doesn't have a readout bus"):
-            experiment_analysis.bus_setup(parameters={Parameter.AMPLITUDE: 0.6})
+            experiment_analysis_1D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6})
+        with pytest.raises(ValueError, match="The experiment doesn't have a readout bus"):
+            experiment_analysis_2D.bus_setup(parameters={Parameter.AMPLITUDE: 0.6})
 
-    def test_control_gate_setup(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_control_gate_setup(self,
+                                experiment_analysis_1D: DummyExperimentAnalysis,
+                                experiment_analysis_2D: DummyExperimentAnalysis):
         """Test the ``control_gate_setup`` method."""
-        assert not hasattr(experiment_analysis, "execution_manager")  # ``build_execution`` has not been called
-        experiment_analysis.gate_setup(gate="X(0)", parameters={Parameter.AMPLITUDE: 123})
-        assert hasattr(experiment_analysis, "execution_manager")  # ``build_execution`` has been called
-        assert experiment_analysis.platform.get_element("X(0)").amplitude == 123
+        assert not hasattr(experiment_analysis_1D, "execution_manager")  # ``build_execution`` has not been called
+        experiment_analysis_1D.gate_setup(gate="X(0)", parameters={Parameter.AMPLITUDE: 123})
+        assert hasattr(experiment_analysis_1D, "execution_manager")  # ``build_execution`` has been called
+        assert experiment_analysis_1D.platform.get_element("X(0)").amplitude == 123
+        
+        assert not hasattr(experiment_analysis_2D, "execution_manager")  # ``build_execution`` has not been called
+        experiment_analysis_2D.gate_setup(gate="X(0)", parameters={Parameter.AMPLITUDE: 123})
+        assert hasattr(experiment_analysis_2D, "execution_manager")  # ``build_execution`` has been called
+        assert experiment_analysis_2D.platform.get_element("X(0)").amplitude == 123
 
-    def test_measurement_setup(self, experiment_analysis: DummyExperimentAnalysis):
+    def test_measurement_setup(self,
+                               experiment_analysis_1D: DummyExperimentAnalysis,
+                               experiment_analysis_2D: DummyExperimentAnalysis):
         """Test the ``measurement_setup`` method."""
-        assert not hasattr(experiment_analysis, "execution_manager")  # ``build_execution`` has not been called
-        experiment_analysis.gate_setup(gate="M(0)", parameters={Parameter.AMPLITUDE: 123})
-        assert hasattr(experiment_analysis, "execution_manager")  # ``build_execution`` has been called
-        assert experiment_analysis.platform.get_element("M(0)").amplitude == 123
+        assert not hasattr(experiment_analysis_1D, "execution_manager")  # ``build_execution`` has not been called
+        experiment_analysis_1D.gate_setup(gate="M(0)", parameters={Parameter.AMPLITUDE: 123})
+        assert hasattr(experiment_analysis_1D, "execution_manager")  # ``build_execution`` has been called
+        assert experiment_analysis_1D.platform.get_element("M(0)").amplitude == 123
+        
+        assert not hasattr(experiment_analysis_2D, "execution_manager")  # ``build_execution`` has not been called
+        experiment_analysis_2D.gate_setup(gate="M(0)", parameters={Parameter.AMPLITUDE: 123})
+        assert hasattr(experiment_analysis_2D, "execution_manager")  # ``build_execution`` has been called
+        assert experiment_analysis_2D.platform.get_element("M(0)").amplitude == 123
