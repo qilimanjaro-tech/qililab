ad86ac1d1ce0d391ec37460c2d41ca4262b97dc3
victor <visangim@gmail.com>
2023-06-13 12:17:27 +0200

    removed randomized benchmarking - oops

--- src/qililab/experiment/portfolio/__init__.py
+++ src/qililab/experiment/portfolio/__init__.py
@@ -4,5 +4,4 @@ from .experiment_analysis import ExperimentAnalysis
 from .fitting_models import *
 from .flipping_sequence import FlippingSequence
 from .rabi import Rabi
-from .randomized_benchmarking import *
 from .t1 import T1
