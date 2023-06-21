e53a2e599a9076e2d75160736525a5a7cff9f7f3
andrea-pema <andrea.perez@qilimanjaro.tech>
2023-06-21 12:34:40 +0200

    lfilter quick fix

--- src/qililab/pulse/pulse_distortion/lfilter_correction.py
+++ src/qililab/pulse/pulse_distortion/lfilter_correction.py
@@ -72,9 +72,18 @@ class LFilterCorrection(PulseDistortion):
             numpy.ndarray: Amplitude of the envelope for each time step.
         """
         # Filtered signal, normalized with envelopes max heights (of the real parts)
-        norm = np.amax(np.real(envelope)) * self.norm_factor
+        if np.median(envelope) >=0:
+            norm = np.amax(np.real(envelope)) * self.norm_factor
+        else:
+            norm = np.amin(np.real(envelope)) * self.norm_factor
+            
         corr_envelope = signal.lfilter(b=self.b, a=self.a, x=envelope)
-        corr_norm = np.max(np.real(corr_envelope))
+        
+        if np.median(envelope) >=0:
+            corr_norm = np.max(np.real(corr_envelope))
+        else:
+            corr_norm = np.min(np.real(corr_envelope))
+            
         corr_envelope = corr_envelope * norm / corr_norm
 
         return corr_envelope
