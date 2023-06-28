33d00414da53aec9d556dd2fc1e4e3fe3e15f640
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-06-28 18:36:42 +0200

    improved test somewhat

--- src/qililab/drivers/instruments/qblox/spi_rack.py
+++ src/qililab/drivers/instruments/qblox/spi_rack.py
@@ -1,11 +1,12 @@
 """Drivers for SpiRack and its corresponging channels: D5aDacChannel & S4gDacChannel."""
 from qblox_instruments import SpiRack
-from qblox_instruments.qcodes_drivers.spi_rack_modules.d5a_module import D5aDacChannel
-from qblox_instruments.qcodes_drivers.spi_rack_modules.s4g_module import S4gDacChannel
+from qblox_instruments.qcodes_drivers.spi_rack_modules.d5a_module import D5aDacChannel, D5aModule
+from qblox_instruments.qcodes_drivers.spi_rack_modules.s4g_module import S4gDacChannel, S4gModule
 
 from qililab.drivers.interfaces import CurrentSource, VoltageSource
 
 
+# MAIN SpiRack CLASS
 class QbloxSpiRack(SpiRack):
     """
     Qililab's driver for the Qblox SpiRack.
@@ -21,9 +22,53 @@ class QbloxSpiRack(SpiRack):
     """
 
 
+# MODULE CLASSES that select the channels
+class QbloxD5aModule(D5aModule):
+    """
+    Qililab's driver for the Qblox D5a Module.
+
+    `QCoDeS <https://qcodes.github.io/Qcodes/>`_ style instrument channel driver for the D5a SPI module.
+
+    Args:
+        parent(SpiRack): Reference to the :class:`~qblox_instruments.SpiRack` parent object. This is handled by
+            the :func:`~qblox_instruments.SpiRack.add_spi_module` function.
+        name (str): Name given to the InstrumentChannel.
+        address (int): Module number set on the hardware.
+        reset_voltages (bool): If True, then reset all voltages to zero and change the span to `range_max_bi`. Default to False.
+        dac_names (Optional[List[str]]): List of all the names to use for the dac channels.
+            If no list is given or is None, the default name "dac{i}" is used for the i-th dac channel. Defaults to None.
+        is_dummy (bool): If true, do not connect to physical hardware, but use a dummy module. Defaults to False.
+
+    Raises:
+        ValueError: Length of the dac names list does not match the number of dacs.
+    """
+
+
+class QbloxS4gModule(S4gModule):
+    """
+    Qililab's driver for the Qblox S4g Module.
+
+    `QCoDeS <https://qcodes.github.io/Qcodes/>`_ style instrument channel driver for the S4g SPI module.
+
+    Args:
+        parent(SpiRack): Reference to the :class:`~qblox_instruments.SpiRack` parent object. This is handled by
+            the :func:`~qblox_instruments.SpiRack.add_spi_module` function.
+        name (str): Name given to the InstrumentChannel.
+        address (int): Module number set on the hardware.
+        reset_voltages (bool): If True, then reset all voltages to zero and change the span to `range_max_bi`. Default to False.
+        dac_names (Optional[List[str]]): List of all the names to use for the dac channels.
+            If no list is given or is None, the default name "dac{i}" is used for the i-th dac channel. Defaults to None.
+        is_dummy (bool): If true, do not connect to physical hardware, but use a dummy module. Defaults to False.
+
+    Raises:
+        ValueError: Length of the dac names list does not match the number of dacs.
+    """
+
+
+# CHANNELS CLASSES that act as the corresponding Voltage/Current sources.
 class QbloxD5aDacChannel(D5aDacChannel, VoltageSource):
     """
-    Qililab's driver for the Qblox D5a Module, D5a DAC Channel x16, acting as VoltageSource.
+    Qililab's driver for the Qblox D5a DAC Channel x16, acting as VoltageSource.
 
     `QCoDeS <https://qcodes.github.io/Qcodes/>`_ style instrument channel driver for the dac channels of the D5a module.
 
@@ -36,7 +81,7 @@ class QbloxD5aDacChannel(D5aDacChannel, VoltageSource):
 
 class QbloxS4gDacChannel(S4gDacChannel, CurrentSource):
     """
-    Qililab's driver for the Qblox S4g Module, S4g DAC Channel x4, acting as CurrentSource.
+    Qililab's driver for the Qblox S4g DAC Channel x4, acting as CurrentSource.
 
     `QCoDeS <https://qcodes.github.io/Qcodes/>`_ style instrument channel driver for the dac channels of the S4g module.
 
--- tests/unit/drivers/instruments/keithley/test_keithley_2600.py
+++ tests/unit/drivers/instruments/keithley/test_keithley_2600.py
@@ -1,4 +1,6 @@
 """ Test classes, Keithley2600 and Keithley2600Channel"""
+from unittest.mock import MagicMock
+
 from qcodes.instrument.instrument import Instrument
 
 from qililab.drivers import Keithley2600, Keithley2600Channel
@@ -9,19 +11,25 @@ class TestOnOff:
     """Test On and off methods we implement in qililab that don't exist in QCoDeS."""
 
     def test_on(self):
-        """Test that need to be improved"""
-        a = Keithley2600Channel(parent=Instrument, name="jesus", channel="smua")
-        b = Keithley2600Channel(parent=Instrument, name="jesus", channel="smub")
-        a.on()
-        b.on()
+        """Test that needs to be improved"""
+        parent = MagicMock()
 
-        assert a.output == b.output == 1
+        a_channel = Keithley2600Channel(parent=parent, name="jesus_a", channel="smua")
+        b_channel = Keithley2600Channel(parent=parent, name="jesus_b", channel="smub")
+
+        a_channel.on()
+        b_channel.on()
+
+        assert a_channel.get(param_name="output") == b_channel.get(param_name="output") == 1
 
     def test_off(self):
-        """Test that need to be improved"""
-        a = Keithley2600Channel(parent=Instrument, name="jesus", channel="smua")
-        b = Keithley2600Channel(parent=Instrument, name="jesus", channel="smub")
-        a.off()
-        b.off()
+        """Test that needs to be improved"""
+        parent = MagicMock()
 
-        assert a.output == b.output == 0
+        a_channel = Keithley2600Channel(parent=parent, name="jesus_a", channel="smua")
+        b_channel = Keithley2600Channel(parent=parent, name="jesus_b", channel="smub")
+
+        a_channel.off()
+        b_channel.off()
+
+        assert a_channel.get(param_name="output") == b_channel.get(param_name="output") == 0
