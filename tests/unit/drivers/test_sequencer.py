84f8ae5cd9291dfccccfc406c630d31fb9f239f8
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-07-03 18:33:00 +0200

    using value of enum

--- tests/unit/drivers/test_sequencer.py
+++ tests/unit/drivers/test_sequencer.py
@@ -130,8 +130,8 @@ class TestSequencer:
         sequencer_name = "test_sequencer_waveforms"
         seq_idx = 0
         expected_waveforms_keys = [
-            f"Gaussian(name=<{PULSE_NAME}: '{PULSE_NAME.lower()}'>, num_sigmas={PULSE_SIGMAS}) - {PULSE_DURATION}ns_I",
-            f"Gaussian(name=<{PULSE_NAME}: '{PULSE_NAME.lower()}'>, num_sigmas={PULSE_SIGMAS}) - {PULSE_DURATION}ns_Q",
+            f"Gaussian(name=<{PULSE_NAME}: '{PULSE_NAME.value.lower()}'>, num_sigmas={PULSE_SIGMAS}) - {PULSE_DURATION}ns_I",
+            f"Gaussian(name=<{PULSE_NAME}: '{PULSE_NAME.value.lower()}'>, num_sigmas={PULSE_SIGMAS}) - {PULSE_DURATION}ns_Q",
         ]
         qcm_qrm = MockQcmQrm(name="test_qcm_qrm_waveforms", slot_idx=0)
         sequencer = AWGSequencer(parent=qcm_qrm, name=sequencer_name, seq_idx=seq_idx)
