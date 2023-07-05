9a428c099ca95067c130bb03d5236a45e0bb1f40
Vyron Vasileiadis <hi@fedonman.com>
2023-07-05 12:04:45 +0200

    initial idea

--- src/qililab/qprogram/blocks/block.py
+++ src/qililab/qprogram/blocks/block.py
@@ -1,2 +1,11 @@
+from dataclasses import dataclass, field
+
+from qililab.qprogram.operations.operation import Operation
+
+
+@dataclass
 class Block:
-    pass
+    elements: list["Block" | Operation] = field(default_factory=list)
+
+    def append(self, element: "Block" | Operation):
+        self.elements.append(element)
