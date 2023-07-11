a4ed0b0642ab413e1013c0aa044cd9384a038805
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-07-11 10:13:41 +0200

    creating bus structure

---
+++ src/qililab/buses/interfaces/bus.py
@@ -0,0 +1,40 @@
+"""Bus Class Interface."""
+from abc import ABC, abstractmethod
+from typing import Any
+
+from qililab.pulse import PulseBusSchedule
+
+class Bus(ABC):
+    """Interface of a Bus."""
+
+    @abstractmethod
+    def execute(
+        self, pulse_bus_schedule: PulseBusSchedule, nshots: int, repetition_duration: int, num_bins: int
+    ) -> None:
+        """Execute a pulse bus schedule through an AWG Sequencer belonging to the bus.
+
+        Args:
+            pulse_bus_schedule (PulseBusSchedule): Pulse Bus Schedule to generate QASM program.
+            nshots (int): number of shots
+            repetition_duration (int): repetition duration.
+            num_bins (int): number of bins
+        """
+
+    @abstractmethod
+    def set(self, param_name: str, value: Any) -> None:
+        """Set parameter on the bus' instruments.
+
+        Args:
+            param (str): Parameter's name.
+            value (float): Parameter's value
+        """
+
+    @abstractmethod
+    def get(self, param_name: str) -> Any:
+        """Return value associated to a parameter on the bus' instrument.
+
+        Args:
+            param (str): Parameter's name.
+        Returns:
+            value (float): Parameter's value
+        """
