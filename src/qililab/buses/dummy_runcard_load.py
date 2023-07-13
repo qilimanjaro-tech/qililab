6ff11058e016637b5a6e03d30f78c1a4eab4fbb2
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-07-13 09:36:59 +0200

    some more progress on a mock up for buses

--- src/qililab/buses/dummy_runcard_load.py
+++ src/qililab/buses/dummy_runcard_load.py
@@ -1,8 +1,19 @@
 from qililab.typings.yaml_type import yaml
 import importlib
-from qililab.buses.instruments.bus import GenericBus
+from qililab.buses.interfaces.bus import BusInterface
 from qililab.buses.instruments.drive_bus import DriveBus
 from qcodes import Instrument
+
+def find_instrument(runcard: dict, key: str, value: str) -> dict:
+    instruments = runcard['instruments']
+    for instrument in instruments.keys():
+        if key in instrument and value in instrument[key]:
+            return instrument
+        elif 'submodules' in instrument:
+            for submodule in instrument['submodules']:
+                if key in submodule and value in submodule[key]:
+                    return instrument
+    return {}
 
 with open("galadriel_test_abs_path.yml", "r") as stream:
     try:
@@ -13,25 +24,24 @@ with open("galadriel_test_abs_path.yml", "r") as stream:
 buses_module_name = "qililab.buses.instruments.drive_bus"
 instruments_module_name = "qililab.drivers.instruments."
 
-buses:list[GenericBus] = []
-instruments:list[Instrument] = []
+buses:list[BusInterface] = []
 
-for instrument in runcard['instruments']:
-    abs_path = instruments_module_name+instrument['type']
-    module_name, class_name = abs_path.rsplit(".", 1)
-    name = instrument['alias']
-    address = instrument['address']
-    InstrumentClass = getattr(importlib.import_module(module_name), class_name)
-    instrumen_instance = InstrumentClass(name, address)
-    instruments.append(instrumen_instance)
+# for instrument in runcard['instruments']:
+#     abs_path = instruments_module_name+instrument['type']
+#     module_name, class_name = abs_path.rsplit(".", 1)
+#     name = instrument['alias']
+#     address = instrument['address']
+#     InstrumentClass = getattr(importlib.import_module(module_name), class_name)
+#     instrumen_instance = InstrumentClass(name, address)
+#     instruments.append(instrumen_instance)
 
-# for bus in runcard['buses']:
-#     class_name = bus['type']
-#     if 'drive' in class_name.lower():
-#         print()
-#         MyClass = getattr(importlib.import_module(module_name), class_name)
-#         instance = MyClass()
-#         print(type(instance))
+for bus in runcard['buses']:
+    class_name = bus['type']
+    if 'drive' in class_name.lower():
+        print()
+        MyClass = getattr(importlib.import_module(module_name), class_name)
+        instance = MyClass()
+        print(type(instance))
 # module_name, class_name = "qililab.buses.instruments.drive_bus.DriveBus".rsplit(".", 1)
 # print(module_name)
 # print(class_name)
