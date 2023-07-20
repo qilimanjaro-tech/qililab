b016c48660a863cb8b11df37386024013660c7b3
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-07-20 10:04:38 +0200

    fixing imports

--- src/qililab/platform/components/interfaces/bus.py
+++ src/qililab/platform/components/interfaces/bus.py
@@ -14,11 +14,17 @@ class BusInterface(ABC):
 
     @abstractmethod
     def execute(
-        self, pulse_bus_schedule: PulseBusSchedule, nshots: int, repetition_duration: int, num_bins: int
+        self,
+        instrument_name: str,
+        pulse_bus_schedule: PulseBusSchedule,
+        nshots: int,
+        repetition_duration: int,
+        num_bins: int,
     ) -> None:
         """Execute a pulse bus schedule through an AWG Sequencer belonging to the bus.
 
         Args:
+            instrument_name: The name of the instrument
             pulse_bus_schedule (PulseBusSchedule): Pulse Bus Schedule to generate QASM program.
             nshots (int): number of shots
             repetition_duration (int): repetition duration.
