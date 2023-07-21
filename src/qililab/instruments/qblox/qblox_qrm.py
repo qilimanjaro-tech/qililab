ce1134b7752eb34eadbe7d8e76b1616850d47b38
Joel Pérez <78150444+JoelPerezDiaz@users.noreply.github.com>
2023-07-21 10:36:30 +0200

    chore(deps): update qpysequence==0.9.2 (#466)
    * chore(deps): update qpysequence==0.9.1
    
    * Update to qpysequence==0.9.2

--- src/qililab/instruments/qblox/qblox_qrm.py
+++ src/qililab/instruments/qblox/qblox_qrm.py
@@ -2,10 +2,11 @@
 from dataclasses import dataclass
 from typing import Sequence, cast
 
+from qpysequence import Program
 from qpysequence import Sequence as QpySequence
-from qpysequence.program import Loop, Program, Register
+from qpysequence import Weights
+from qpysequence.program import Loop, Register
 from qpysequence.program.instructions import Acquire, AcquireWeighed, Move
-from qpysequence.weights import Weights
 
 from qililab.config import logger
 from qililab.instruments.awg_analog_digital_converter import AWGAnalogDigitalConverter
