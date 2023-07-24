d8782cbc8cdebc6bb766fdeb472ad46003d49e8e
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-07-24 17:32:34 +0200

    merging main into new-main

--- src/qililab/experiment/portfolio/__init__.py
+++ src/qililab/experiment/portfolio/__init__.py
@@ -1,7 +1,7 @@
 """This submodule contains a portfolio of pre-defined experiments."""
 from .experiment_analysis import ExperimentAnalysis
-#from .fitting_models import *
+from .fitting_models import *
 from .flipping_sequence import FlippingSequence
 from .rabi import Rabi
-#from .t1 import T1
+from .t1 import T1
 from .t2echo import T2Echo
