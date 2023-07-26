82fda797761747f21424013e7659debdb31aeb2d
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-07-26 11:23:34 +0200

    main addition

--- src/qililab/drivers/instruments/__init__.py
+++ src/qililab/drivers/instruments/__init__.py
@@ -1,5 +1,7 @@
+"""__init__.py"""
 from .era_synth import ERASynthPlus
 from .keithley import Keithley2600
 from .qblox import Cluster, Pulsar, SpiRack
 from .rohde_schwarz import RhodeSchwarzSGS100A
+from .utils import InstrumentFactory
 from .yokogawa import GS200
---
+++ src/qililab/drivers/instruments/utils/instrument_factory.py
@@ -0,0 +1,28 @@
+"""InstrumentFactory class."""
+from typing import TypeVar
+
+from qililab.instruments.instrument import Instrument
+from qililab.typings.enums import InstrumentName
+
+Element = TypeVar("Element", bound=Instrument)
+
+
+class InstrumentFactory:
+    """Hash table that loads a specific class given an object's name."""
+
+    handlers: dict[str, type[Instrument]] = {}
+
+    @classmethod
+    def register(cls, handler_cls: type[Element]) -> type[Instrument]:
+        """Register handler in the factory.
+
+        Args:
+            output_type (type): Class type to register.
+        """
+        cls.handlers[handler_cls.name.value] = handler_cls
+        return handler_cls
+
+    @classmethod
+    def get(cls, name: str | InstrumentName) -> type[Instrument]:
+        """Return class attribute."""
+        return cls.handlers[name.value] if isinstance(name, InstrumentName) else cls.handlers[name]
