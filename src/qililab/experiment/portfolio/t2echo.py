5179f7a8475ad38f1866dbcd19a5f5f0150ae9ce
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-08-03 10:52:20 +0200

    Fix all warnings

--- src/qililab/experiment/portfolio/t2echo.py
+++ src/qililab/experiment/portfolio/t2echo.py
@@ -14,17 +14,18 @@ from qililab.utils import Loop, Wait
 class T2Echo(ExperimentAnalysis, Exp):
     """Class used to run a T2Echo experiment on the given qubit. The experiment performs the following circuit:
 
-    1. Prepare the qubit in the ∣-i⟩ state by sending a π/2-pulse to the qubit.
-    2. Wait some time t.
-    4. Send a π-pulse to the qubit.
-    5. Wait some time t.
-    6. Send a π/2-pulse to the qubit.
-    7. Measure the qubit.
+    1. Prepare the qubit in the ∣-i⟩ state by sending a π/2-pulse to the qubit
+    2. Wait some time t
+    3. Send a π-pulse to the qubit
+    4. Wait some time t
+    5. Send a π/2-pulse to the qubit
+    6. Measure the qubit
 
     Args:
         platform (Platform): platform used to run the experiment.
         qubit (int): qubit index used in the experiment.
-        wait_loop_values (ndarray): array of time values to wait for the `Wait` gates, the same value is used on both gates.
+        wait_loop_values (ndarray): array of time values to wait for the `Wait` gates, the same value is used on both
+            gates.
         repetition_duration (int, optional): duration of a single repetition in nanoseconds. Default to 10000.
         measurement_buffer (int, optional): time to wait before taking the measurement. Default to 100.
         hardware_average (int, optional): number of repetitions used to average the result. Default to 10000.
