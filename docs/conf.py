9c5d5c2ce174f4791039730f7833b219a05d9a11
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-08-10 17:50:43 +0200

    Change code block style.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "releases/*"]
 source_suffix = [".rst", ".md"]
-pygments_style = "one-dark"
+pygments_style = "default"
 
 # Napoleon settings
 napoleon_google_docstring = True
