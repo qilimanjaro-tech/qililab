215c4721f1f292f44bb44bb4f170842b5efe0b32
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-08-24 15:30:52 +0200

    black formatting

--- src/qililab/instruments/utils/loader.py
+++ src/qililab/instruments/utils/loader.py
@@ -11,7 +11,9 @@ from qililab.instruments.utils.instrument_reference import InstrumentReference
 class Loader:
     """Loads the Instruments supported types froom the Instrument References"""
 
-    def _get_instrument_or_raise_error_when_not_found_or_not_supported_type(self, instruments: Instruments | NewInstruments, alias: str):
+    def _get_instrument_or_raise_error_when_not_found_or_not_supported_type(
+        self, instruments: Instruments | NewInstruments, alias: str
+    ):
         """get instrument or raise error when not found"""
         instrument = instruments.get_instrument(alias=alias)
         if instrument is None:
