91fac988971a7054110982c8d55188f38fd50044
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-08-25 18:18:47 +0200

    black formatting

--- src/qililab/platform/platform.py
+++ src/qililab/platform/platform.py
@@ -403,14 +403,12 @@ class Platform:  # pylint: disable = too-many-public-methods, too-many-instance-
                 | instrument_dict
                 | instrument_controllers_dict
             )
-        
+
         name_dict = {RUNCARD.NAME: self.name}
         device_id = {RUNCARD.DEVICE_ID: self.device_id}
         gates_settings_dict = {RUNCARD.GATES_SETTINGS: asdict(self.gates_settings, dict_factory=dict_factory)}
         chip_dict = {RUNCARD.CHIP: self.chip.to_dict() if self.chip is not None else None}
-        instrument_dict = {
-            RUNCARD.INSTRUMENTS: self.instruments.to_dict() if self.instruments is not None else None
-        }
+        instrument_dict = {RUNCARD.INSTRUMENTS: self.instruments.to_dict() if self.instruments is not None else None}
         return (
             name_dict
             | device_id
