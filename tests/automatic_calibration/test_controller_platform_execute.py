e7a8abc260bd4bb603f80cf7740a3189039588de
Marco Paradina <marco.paradina@qilimanjaro.tech>
2023-08-25 19:17:44 +0200

    Rename graph

--- tests/automatic_calibration/test_controller_platform_execute.py
+++ tests/automatic_calibration/test_controller_platform_execute.py
@@ -168,7 +168,7 @@ nodes = [
 
 calibration_graph.add_nodes_from(nodes)
 
-calibration_graph_figure_path = "./tests/automatic_calibration/calibration_graph.PNG"
+calibration_graph_figure_path = "./tests/automatic_calibration/pulse_schedule_1_node.PNG"
 
 # Visualization of the calibration graph
 #visualize_calibration_graph(calibration_graph = calibration_graph, graph_figure_path = calibration_graph_figure_path)
