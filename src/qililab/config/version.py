4a6aa11a00a66999b1ec0c0e9abf5a37b7b77d46
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-09-12 12:09:26 +0200

    Remove useless files.

--- src/qililab/config/version.py
+++ src/qililab/config/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" library version """
+"""Version number (major.minor.patch[-label])"""
 __version__ = "0.20.4"
