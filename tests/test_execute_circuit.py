9794fb4e459407b37e8e03656b3891c186b80b33
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-10-03 15:31:42 +0200

    test: :test_tube: Fix tests.

--- tests/test_execute_circuit.py
+++ tests/test_execute_circuit.py
@@ -7,38 +7,38 @@ from qibo.models import Circuit
 
 import qililab as ql
 
-mock_platform = MagicMock()
-
 
 class TestExecute:
     """Test execute method in the `execute.py` script"""
 
-    @patch("qililab.execute_circuit.build_platform", return_value=mock_platform)
-    def test_execute(self, mock_build: MagicMock):
+    def test_execute(self):
         n_qubits = 5
         circuit = Circuit(n_qubits)
         circuit.add([X(1), H(2), RY(0, 2), CNOT(4, 1), X(4), H(3), M(*range(5))])
         runcard = "galadriel.yml"
-        ql.execute(circuit, runcard)
-        mock_build.assert_called_with(runcard="galadriel.yml")
+        mock_platform = MagicMock()
+        with patch("qililab.execute_circuit.build_platform", return_value=mock_platform) as mock_build:
+            ql.execute(circuit, runcard)
+            mock_build.assert_called_with(runcard="galadriel.yml")
         mock_platform.connect.assert_called_once_with()
         mock_platform.initial_setup.assert_called_once_with()
         mock_platform.turn_on_instruments.assert_called_once_with()
         mock_platform.execute.assert_called_once_with(circuit, num_avg=1, repetition_duration=200_000, num_bins=1)
         mock_platform.disconnect.assert_called_once_with()
 
-    @patch("qililab.execute_circuit.build_platform", return_value=mock_platform)
-    def test_execute_calls_disconnect_after_error(self, mock_build: MagicMock):
+    def test_execute_calls_disconnect_after_error(self):
         n_qubits = 5
         circuit = Circuit(n_qubits)
         circuit.add([X(1), H(2), RY(0, 2), CNOT(4, 1), X(4), H(3), M(*range(5))])
         runcard = "galadriel.yml"
-        mock_platform.turn_on_instruments.side_effect = Exception()
         with pytest.raises(Exception):
-            ql.execute(circuit, runcard)
-        mock_build.assert_called_with(runcard="galadriel.yml")
-        mock_platform.connect.assert_called_once_with()
-        mock_platform.initial_setup.assert_called_once_with()
-        mock_platform.turn_on_instruments.assert_called_once_with()
-        mock_platform.execute.assert_not_called()
-        mock_platform.disconnect.assert_called_once_with()
+            mock_platform = MagicMock()
+            mock_platform.turn_on_instruments.side_effect = Exception()
+            with patch("qililab.execute_circuit.build_platform", return_value=mock_platform) as mock_build:
+                ql.execute(circuit, runcard)
+            mock_build.assert_called_with(runcard="galadriel.yml")
+            mock_platform.connect.assert_called_once_with()
+            mock_platform.initial_setup.assert_called_once_with()
+            mock_platform.turn_on_instruments.assert_called_once_with()
+            mock_platform.execute.assert_not_called()
+            mock_platform.disconnect.assert_called_once_with()
