b79e895cb7a80d4a626e441d61d2168d54ebca5b
victor <visangim@gmail.com>
2023-10-04 18:34:35 +0200

    fix unit tests

--- src/qililab/settings/gate_event_settings.py
+++ src/qililab/settings/gate_event_settings.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass
 
 from qililab.typings.enums import Parameter
 
@@ -46,11 +46,11 @@ class GateEventSettings:
         phase: float
         duration: int
         shape: dict
-        options: dict = field(default_factory=dict)
+        options: InitVar[dict | None] = None
 
-        def __post_init__(self):
-            for key, value in self.options.items():
-                setattr(self, key, value)
+        def __post_init__(self, options):
+            if options is not None:
+                self.options = options
 
     bus: str
     pulse: GatePulseSettings
