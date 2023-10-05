87f66dfd7b3807728b4922b0a343c22be0a4eed8
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-10-05 21:30:57 +0200

    main commit

--- src/qililab/automatic_calibration/current_examples/test_autocalibration.py
+++ src/qililab/automatic_calibration/current_examples/test_autocalibration.py
@@ -13,9 +13,15 @@ os.chdir(dname)
 path_runcard = abspath.split("qililab")[0] + "qililab/examples/runcards/galadriel.yml"
 
 # CREATE NODES:
-first = CalibrationNode(nb_path="notebooks/first.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1800.0)
-second = CalibrationNode(nb_path="notebooks/second.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1.0)
-third = CalibrationNode(nb_path="notebooks/third.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1.0)
+first = CalibrationNode(
+    nb_path="notebooks/first.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1800.0
+)
+second = CalibrationNode(
+    nb_path="notebooks/second.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1.0
+)
+third = CalibrationNode(
+    nb_path="notebooks/third.ipynb", in_spec_threshold=0.1, bad_data_threshold=0.2, drift_timeout=1.0
+)
 
 # NODE MAPPING TO THE GRAPH (key = name in graph, value = node object):
 nodes = {"first": first, "second": second, "third": third}
@@ -30,26 +36,32 @@ controller = CalibrationController(node_sequence=nodes, calibration_graph=G, run
 
 
 ### EXTRA THINGS, WHICH WILL NOT BE NEEDED IN THE FUTURE:
-first.output_parameters = {"check_parameters": {
+first.output_parameters = {
+    "check_parameters": {
         "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-        "y": [2, 1, 3, 42, 5, 6, 7, 8, 9, 10, 11, 12, 13, 114, 15, 16, 17, 18, 19]
-}}
-second.output_parameters = {"check_parameters": {
-    "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    "y": [2, 1, 3, 14, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 168, 17, 18, 19],
-}}
-third.output_parameters = {"check_parameters": {
+        "y": [2, 1, 3, 42, 5, 6, 7, 8, 9, 10, 11, 12, 13, 114, 15, 16, 17, 18, 19],
+    }
+}
+second.output_parameters = {
+    "check_parameters": {
+        "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        "y": [2, 1, 3, 14, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 168, 17, 18, 19],
+    }
+}
+third.output_parameters = {
+    "check_parameters": {
         "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
         "y": [2, 1, 3, 4, 15, 6, 7, 8, 9, 10, 11, 12, 13, 4, 15, 16, 17, 18, 19],
-}}
+    }
+}
 
 
 ### EXECUTIONS TO DO:
 
 # Calibrate the nodes, for when we have fucked up:
-# controller.calibrate(first)
-# controller.calibrate(second)
-# controller.calibrate(third)
+controller.calibrate(first)
+controller.calibrate(second)
+controller.calibrate(third)
 
 # Maintain from highest node:
 controller.maintain(third)
