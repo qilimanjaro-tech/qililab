f8ca72266518d3de74aad5bb261231f9bdcb4122
victor <visangim@gmail.com>
2023-10-09 18:52:24 +0200

    add cz to data

--- tests/data.py
+++ tests/data.py
@@ -139,6 +139,18 @@ class Galadriel:
                     },
                 }
             ],
+            "CZ(0,1)": [
+                {
+                    "bus": "flux_line_q1_bus",
+                    "wait_time": 0,
+                    "pulse": {
+                        "amplitude": 1.0,
+                        "phase": 1.5707963267948966,
+                        "duration": 20,
+                        "shape": {"name": "rectangular"},
+                    },
+                }
+            ],
         },
     }
 
