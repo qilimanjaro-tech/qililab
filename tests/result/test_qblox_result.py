5e33dfc3621b30bafd5c1c100dbd9084e2c4342f
victor <visangim@gmail.com>
2023-10-31 16:24:43 +0100

    Revert "fixing tests"
    This reverts commit 8c6dad5538923c004f821e55aa8ef76c389e0133.

--- tests/result/test_qblox_result.py
+++ tests/result/test_qblox_result.py
@@ -113,8 +113,6 @@ def fixture_qblox_asymmetric_bins_result():
                 "threshold": [0.0, 1.0],
                 "avg_cnt": [1, 1],
             },
-            "qubit": 0,
-            "measurement": 0,
         },
         {
             "scope": {
@@ -126,8 +124,6 @@ def fixture_qblox_asymmetric_bins_result():
                 "threshold": [1.0, 0.0, 1.0],
                 "avg_cnt": [1, 1, 1],
             },
-            "qubit": 0,
-            "measurement": 1,
         },
     ]
     return QbloxResult(integration_lengths=[1000, 1000], qblox_raw_results=qblox_raw_results)
@@ -275,7 +271,7 @@ class TestsQbloxResult:
             qblox_asymmetric_bins_result (QbloxResult): QbloxResult instance with different number of bins on each sequencer.
         """
         with pytest.raises(IndexError, match="Sequencers must have the same number of bins."):
-            qblox_asymmetric_bins_result.array()
+            qblox_asymmetric_bins_result.counts_object()
 
     def test_array_property_of_scope(self, dummy_qrm: Pulsar, qblox_result_scope: QbloxResult):
         """Test the array property of the QbloxResult class."""
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,7 +16,7 @@ def mock_instruments(mock_rs: MagicMock, mock_pulsar: MagicMock, mock_keithley: 
     mock_pulsar_instance = mock_pulsar.return_value
     mock_pulsar_instance.get_acquisitions.side_effect = lambda sequencer: copy.deepcopy(
         {
-            "q0_0": {
+            "default": {
                 "index": 0,
                 "acquisition": {
                     "scope": {
