e2f3a50e485bfb6d7d1fb06ccef7761f9e6df904
ziiiki <adriablancoculla@gmail.com>
2023-11-14 14:38:27 +0100

    improve unit testing

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -1,7 +1,12 @@
+import os
+import shutil
+import time
+
 import pytest
 from IPython.testing.globalipapp import start_ipython
 
 # pylint: disable=redefined-outer-name
+slurm_job_data_test = "./slurm_job_data_test"
 
 
 @pytest.fixture(scope="session")
@@ -21,41 +26,35 @@ def test_submit_job(ip):
     ip.run_cell(raw_cell="a=1\nb=1")
     ip.run_cell_magic(
         magic_name="submit_job",
-        line="-o results -d debug -l slurm_job_data_tests -n unit_test -e local",
+        line=f"-o results -d debug -l {slurm_job_data_test} -n unit_test -e local",
         cell="results = a+b ",
     )
     assert ip.user_global_ns["results"].result() == 2
+    shutil.rmtree(slurm_job_data_test)  # manual teardown
 
 
 def test_submit_job_output_not_assigned(ip):
     """Check ValueError is raised in case values are not assigned to the output variable within the magic cell."""
     ip.run_cell(raw_cell="a=1\nb=1")
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="Output variable 'results' was not assigned to any value inside the cell!"):
         ip.run_cell_magic(
             magic_name="submit_job",
-            line="-o results -d debug -l slurm_job_data_tests -n unit_test -e local",
+            line=f"-o results -d debug -l {slurm_job_data_test} -n unit_test -e local",
             cell="a+b",
         )
+    shutil.rmtree(slurm_job_data_test)  # manual teardown
 
 
 def test_submit_job_with_random_file_in_logs_folder(ip):
-    """Check ValueError is raised if non-submitit files are found in logs folder"""
+    """Check non-submitit files are deleted if found in logs folder"""
     ip.run_cell(
-        raw_cell="import os\nfolder_path='slurm_job_data_tests'\nfile_name='abc.py'\nfile_path = os.path.join(folder_path, file_name)\nif not os.path.exists(folder_path):\tos.makedirs(folder_path)\nopen(file_path, 'w')\na=1\nb=1"
+        raw_cell=f"import os\nfolder_path='{slurm_job_data_test}'\nfile_name='abc.py'\nfile_path = os.path.join(folder_path, file_name)\nif not os.path.exists(folder_path):\tos.makedirs(folder_path)\nopen(file_path, 'w')\na=1\nb=1"
     )
     ip.run_cell_magic(
         magic_name="submit_job",
-        line="-o results -d debug -l slurm_job_data_tests -n unit_test -e local",
+        line=f"-o results -d debug -l {slurm_job_data_test} -n unit_test -e local",
         cell="results=a+b",
     )
-
-
-def test_submit_job_delete_info_from_past_jobs(ip):
-    """Check ValueError is raised if non-submitit files are found in logs folder"""
-    ip.run_cell(raw_cell="a=1\nb=1")
-    for _ in range(11):
-        ip.run_cell_magic(
-            magic_name="submit_job",
-            line="-o results -d debug -l slurm_job_data_tests -n unit_test -e local",
-            cell="results=a+b",
-        )
+    assert os.path.isfile(os.path.join(slurm_job_data_test, "abc.py")) is False
+    time.sleep(2)  # give time to ensure processes are finished
+    shutil.rmtree(slurm_job_data_test)  # manual teardown
