e12edb52b4ab96bdbd211d19202f26b29a9b836d
Vyron Vasileiadis <hi@fedonman.com>
2023-11-29 12:29:32 +0100

    Update YokogawaGS200 Controller tests

--- tests/instrument_controllers/yokogawa/test_yokogawa_gs200_controller.py
+++ tests/instrument_controllers/yokogawa/test_yokogawa_gs200_controller.py
@@ -3,9 +3,11 @@ import copy
 import pytest
 
 from qililab.instrument_controllers.yokogawa.gs200_controller import GS200Controller
+from qililab.instruments.yokogawa.gs200 import GS200
 from qililab.platform import Platform
-from tests.data import SauronYokogawa
-from tests.test_utils import build_platform
+from qililab.settings import Settings
+from tests.data import SauronYokogawa  # pylint: disable=import-error
+from tests.test_utils import build_platform  # pylint: disable=import-error
 
 
 @pytest.fixture(name="platform")
@@ -24,6 +26,19 @@ def fixture_yokogawa_gs200_controller_wrong_module(platform: Platform):
 
 class TestYokogawaGS200Controller:
     """Unit tests checking the GS200 Controller attributes and methods"""
+
+    def test_initialization(self, platform: Platform):
+        """Test QMMController has been initialized correctly"""
+        controller_alias = "yokogawa_controller"
+        controller_instance = platform.instrument_controllers.get_instrument_controller(alias=controller_alias)
+        assert isinstance(controller_instance, GS200Controller)
+
+        controller_settings = controller_instance.settings
+        assert isinstance(controller_settings, Settings)
+
+        controller_modules = controller_instance.modules
+        assert len(controller_modules) == 1
+        assert isinstance(controller_modules[0], GS200)
 
     def test_check_supported_modules_raises_exception(self, yokogawa_gs200_controller_wrong_module: GS200Controller):
         with pytest.raises(ValueError):
