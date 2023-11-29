7a2bb023b2c089ba793e0edafe0481ce12746e86
Vyron Vasileiadis <hi@fedonman.com>
2023-11-29 16:26:59 +0100

    Add licence

--- src/qililab/instruments/yokogawa/gs200.py
+++ src/qililab/instruments/yokogawa/gs200.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""GS200 class"""
+"""Yokogawa GS200 Instrument"""
 from dataclasses import dataclass
 
 from qililab.instruments.current_source import CurrentSource
