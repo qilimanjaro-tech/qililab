1153e5b9e074b4f3240f10ba6f6530cf5c9381f9
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-12-20 17:17:06 +0100

    Remove unused imports.

--- src/qililab/drivers/instruments/qblox/sequencer_qcm.py
+++ src/qililab/drivers/instruments/qblox/sequencer_qcm.py
@@ -12,12 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any
-
 import numpy as np
 from qblox_instruments.qcodes_drivers.sequencer import Sequencer
-from qcodes import Instrument
-from qcodes import validators as vals
 from qpysequence.acquisitions import Acquisitions
 from qpysequence.library import long_wait
 from qpysequence.program import Block, Loop, Program, Register
