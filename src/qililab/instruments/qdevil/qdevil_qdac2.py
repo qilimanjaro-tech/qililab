42b7fe896bbf7309b952c11fa794e033c4629add
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2023-12-21 20:50:02 +0100

    solved bugs

--- src/qililab/instruments/qdevil/qdevil_qdac2.py
+++ src/qililab/instruments/qdevil/qdevil_qdac2.py
@@ -54,7 +54,7 @@ class QDevilQDac2(VoltageSource):
         """
         return self.settings.low_pass_filter
 
-    def setup(
+    def setup(  # pylint: too-many-branches
         self,
         parameter: Parameter,
         value: float | str | bool,
