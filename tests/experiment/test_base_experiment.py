c8ca6dce1da5d80f02c9a9a7da462ce9aadeaf42
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2023-12-28 00:05:54 +0100

    adjusting magic number to fix test

--- tests/experiment/test_base_experiment.py
+++ tests/experiment/test_base_experiment.py
@@ -349,7 +349,7 @@ class TestReset:
         exp.platform.connect()
         exp.platform.disconnect()
         mock_reset.assert_called()
-        assert mock_reset.call_count == 12
+        assert mock_reset.call_count == 10
 
     @patch("qililab.instrument_controllers.qblox.qblox_pulsar_controller.Pulsar", autospec=True)
     @patch("qililab.instrument_controllers.rohde_schwarz.sgs100a_controller.RohdeSchwarzSGS100A", autospec=True)
