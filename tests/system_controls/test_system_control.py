38a65b1910a51010a41cdebea49d55619c19447f
victor <visangim@gmail.com>
2024-01-05 13:24:18 +0100

    fix coverage

--- tests/system_controls/test_system_control.py
+++ tests/system_controls/test_system_control.py
@@ -1,11 +1,13 @@
 """Tests for the SystemControl class."""
+import re
 from unittest.mock import MagicMock
 
 import pytest
 from qpysequence import Acquisitions, Program, Sequence, Waveforms, Weights
 
 import qililab as ql
-from qililab.instruments import AWG, Instrument
+from qililab.instruments import AWG, Instrument, ParameterNotFound
+from qililab.instruments.qblox import QbloxModule
 from qililab.instruments.rohde_schwarz import SGS100A
 from qililab.platform import Platform
 from qililab.pulse import Gaussian, Pulse, PulseBusSchedule, PulseEvent, PulseSchedule
@@ -39,6 +41,13 @@ def fixture_pulse_schedule() -> PulseSchedule:
 def fixture_system_control(platform: Platform):
     """Fixture that returns an instance of a SystemControl class."""
     settings = {"instruments": ["QRM", "rs_1"]}
+    return SystemControl(settings=settings, platform_instruments=platform.instruments)
+
+
+@pytest.fixture(name="system_control_qcm")
+def fixture_system_control_qcm(platform: Platform):
+    """Fixture that returns an instance of a SystemControl class."""
+    settings = {"instruments": ["QCM", "rs_1"]}
     return SystemControl(settings=settings, platform_instruments=platform.instruments)
 
 
@@ -91,6 +100,26 @@ class TestMethods:
         for instrument in system_control:
             assert isinstance(instrument, Instrument)
 
+    def test_upload_qpysequence(self, system_control: SystemControl, qpysequence: Sequence):
+        awg = system_control.instruments[0]
+        assert isinstance(awg, AWG)
+        awg.device = MagicMock()
+        system_control.upload_qpysequence(qpysequence=qpysequence, port="feedline_input")
+        for seq_idx in range(awg.num_sequencers):
+            # qrm has 2 sequencers and since device is a magic mock, the mock device registers n calls
+            #  of sequence no matter the seq_idx, where n is the number of sequencers in the module
+            assert awg.device.sequencers[seq_idx].sequence.call_count == 2
+
+    def test_upload_qpysequence_raises_error_when_awg_is_missing(
+        self, system_control_without_awg: SystemControl, qpysequence: Sequence
+    ):
+        """Test that the ``upload`` method raises an error when the system control doesn't have an AWG."""
+        with pytest.raises(
+            AttributeError,
+            match="The system control doesn't have any AWG to upload a qpysequence.",
+        ):
+            system_control_without_awg.upload_qpysequence(qpysequence=qpysequence, port="feedline_input")
+
     def test_upload(self, platform: Platform, pulse_schedule: PulseSchedule, system_control: SystemControl):
         """Test upload method."""
         awg = platform.instruments.elements[1]
@@ -109,7 +138,7 @@ class TestMethods:
         ):
             system_control_without_awg.run(port="feedline_input")
 
-    def test_set_parameter(self, system_control: SystemControl):
+    def test_set_parameter_device(self, system_control: SystemControl):
         """Test the ``set_parameter`` method with a Rohde & Schwarz instrument."""
         for instrument in system_control.instruments:
             instrument.device = MagicMock()
@@ -119,6 +148,32 @@ class TestMethods:
                 instrument.device.frequency.assert_called_once_with(1e9)  # type: ignore
             else:
                 instrument.device.frequency.assert_not_called()  # type: ignore
+
+    def test_set_parameter_no_device(self, system_control_qcm: SystemControl):
+        """Test the ``set_parameter`` method with a qblox module without device."""
+        system_control_qcm.set_parameter(parameter=ql.Parameter.IF, value=12.0e06, port_id="drive_q0")
+        for instrument in system_control_qcm.instruments:
+            if isinstance(instrument, QbloxModule):
+                assert instrument.awg_sequencers[0].intermediate_frequency == 12.0e06
+
+    def test_set_parameter_error(self, system_control_qcm: SystemControl):
+        """Test the ``set_parameter`` method with an invalid parameter and check that it raises an error."""
+        param = ql.Parameter.GATE_OPTIONS
+        error_string = re.escape(
+            f"Could not find parameter {param.value} in the system control {system_control_qcm.name}"
+        )
+        with pytest.raises(ParameterNotFound, match=error_string):
+            system_control_qcm.set_parameter(parameter=param, value=12.0e06, port_id="drive_q0")
+
+    def test_get_parameter_error(self, system_control_qcm: SystemControl):
+        """Test the ``get_parameter`` method with an invalid parameter and check that it raises an error."""
+        param = ql.Parameter.GATE_OPTIONS
+        error_string = re.escape(
+            f"Could not find parameter {param.value} in the system control {system_control_qcm.name}"
+        )
+
+        with pytest.raises(ParameterNotFound, match=error_string):
+            system_control_qcm.get_parameter(parameter=param, port_id="drive_q0")
 
 
 class TestProperties:  # pylint: disable=too-few-public-methods
