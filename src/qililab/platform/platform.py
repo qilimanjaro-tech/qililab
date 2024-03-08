21a7450a70c38e251f5b19ec727844040e79d412
jordivallsq <jordi.valls@qilimanjaro.tech>
2024-03-08 10:05:19 +0100

    Trying to reduce the number of branches

--- src/qililab/platform/platform.py
+++ src/qililab/platform/platform.py
@@ -672,8 +672,7 @@ class Platform:  # pylint: disable = too-many-public-methods, too-many-instance-
         # Reset instrument settings
         for bus_alias in sequences:
             for instrument in buses[bus_alias].system_control.instruments:
-                if isinstance(instrument, QbloxModule):
-                    instrument.desync_by_port(buses[bus_alias].port)
+                if isinstance(instrument, QbloxModule): instrument.desync_by_port(buses[bus_alias].port)
 
         return results
 
