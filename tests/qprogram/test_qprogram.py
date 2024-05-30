c85bda6d9f5c56ec3dfdf6ed93ba5c258d8319c9
victor <visangim@gmail.com>
2024-05-30 16:04:14 +0200

    fix tests

--- tests/qprogram/test_qprogram.py
+++ tests/qprogram/test_qprogram.py
@@ -180,6 +180,15 @@ class TestQProgram:
         assert np.equal(qp._body.elements[0].weights.I, one_wf)
         assert np.equal(qp._body.elements[0].weights.Q, zero_wf)
 
+    def test_measure_error_int_length_and_weight_defined(self):
+        """Test that measure raises an error if weights and integration length are given at
+        the same time"""
+        qp = QProgram()
+        drag_wf = IQPair.DRAG(amplitude=1.0, duration=100, num_sigmas=5, drag_coefficient=1.5)
+        weight_wf = IQPair(I=Square(1.0, duration=200), Q=Square(0.0, duration=200))
+        with pytest.raises(ValueError, match="Weights and integration length cannot be defined at the same time."):
+            qp.measure(bus="mock_bus", waveform=drag_wf, weights=weight_wf, integration_length=100)
+
     def test_acquire_method(self):
         """Test acquire method"""
         one_wf = Square(amplitude=1.0, duration=40)
