b52b17a6635f13ed7de29144f3d93d8cecc3b662
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2024-06-10 18:00:27 +0200

    refactor parametrized_experiment_params

--- tests/data.py
+++ tests/data.py
@@ -652,6 +652,18 @@ class Galadriel:
     }
 
 
+parametrized_experiment_params: list[list[str | Circuit | list[Circuit]]] = []
+for platform in [Galadriel]:
+    circuit = Circuit(1)
+    circuit.add(I(0))
+    circuit.add(Align(0))  # Parametrized gate
+    circuit.add(X(0))
+    circuit.add(Y(0))
+    if platform == Galadriel:
+        circuit.add(M(0))
+    parametrized_experiment_params.extend([[platform.runcard, circuit], [platform.runcard, [circuit, circuit]]])  # type: ignore
+
+
 experiment_params: list[list[str | Circuit | list[Circuit]]] = []
 for platform in [Galadriel]:
     circuit = Circuit(1)
@@ -664,17 +676,6 @@ for platform in [Galadriel]:
     if platform == Galadriel:
         circuit.add(M(0))
     experiment_params.extend([[platform.runcard, circuit], [platform.runcard, [circuit, circuit]]])  # type: ignore
-
-parametrized_experiment_params: list[list[str | Circuit | list[Circuit]]] = []
-for platform in [Galadriel]:
-    circuit = Circuit(1)
-    circuit.add(I(0))
-    circuit.add(Align(0))  # Parametrized gate
-    circuit.add(X(0))
-    circuit.add(Y(0))
-    if platform == Galadriel:
-        circuit.add(M(0))
-    parametrized_experiment_params.extend([[platform.runcard, circuit], [platform.runcard, [circuit, circuit]]])  # type: ignore
 
 results_two_loops: dict[str, Any] = {
     EXPERIMENT.SOFTWARE_AVERAGE: 1,
