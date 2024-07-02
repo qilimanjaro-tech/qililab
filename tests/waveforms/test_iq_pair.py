a776af2d6e20efb5db4e3c62c80775d4f5938861
jvallsq <jordi.valls@qilimanjaro.tech>
2024-07-02 17:14:40 +0200

    Updated code coverage

--- tests/waveforms/test_iq_pair.py
+++ tests/waveforms/test_iq_pair.py
@@ -26,6 +26,14 @@ class TestIQPair:
         with pytest.raises(ValueError, match="Waveforms of an IQ pair must have the same duration."):
             IQPair(I=Square(amplitude=0.5, duration=200), Q=Square(amplitude=1.0, duration=100))
 
+    def test_iq_pair_without_waveform_type_throws_error(self):
+        """Test that waveforms of an IQ pair must have Waveform type."""
+        with pytest.raises(ValueError, match="Waveform inside IQPair must have Waveform type."):
+            IQPair(
+                I=IQPair(I=Square(amplitude=0.5, duration=100), Q=Square(amplitude=1.0, duration=100)),
+                Q=IQPair(I=Square(amplitude=0.5, duration=100), Q=Square(amplitude=1.0, duration=100)),
+            )
+
     def test_drag_method(self):
         """Test __init__ method"""
         drag = IQPair.DRAG(drag_coefficient=0.4, amplitude=0.7, duration=40, num_sigmas=2)
