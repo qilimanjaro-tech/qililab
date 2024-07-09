aa3310cdd01e5d9dfbaa1762ca0bcc570585db93
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2024-07-09 16:11:04 +0200

    add probabilities method

--- src/qililab/result/qprogram/__init__.py
+++ src/qililab/result/qprogram/__init__.py
@@ -1,4 +1,4 @@
 from .measurement_result import MeasurementResult
 from .qblox_measurement_result import QbloxMeasurementResult
-from .qprogram_results import QProgramResults
+from .qprogram_results import QProgramResults, probabilities
 from .quantum_machines_measurement_result import QuantumMachinesMeasurementResult
