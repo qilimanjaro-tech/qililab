db6e77132b92b7b33ef6c7b7b6a6d123d0ba4fc0
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2024-08-01 15:15:40 +0200

    code quality improvement

--- src/qililab/instruments/quantum_machines/quantum_machines_cluster.py
+++ src/qililab/instruments/quantum_machines/quantum_machines_cluster.py
@@ -319,9 +319,9 @@ class QuantumMachinesCluster(Instrument):
         for element in elements:
             self._qm.calibrate_element(element)
 
-    def set_parameter_of_bus(
+    def set_parameter_of_bus(  # pylint: disable=too-many-locals
         self, bus: str, parameter: Parameter, value: float | str | bool
-    ) -> None:  # noqa: C901  pylint: disable=too-many-locals
+    ) -> None:  # noqa: C901
         """Sets the parameter of the instrument into the cache (runtime dataclasses).
 
         And if connection to instruments is established, then to the instruments as well.
