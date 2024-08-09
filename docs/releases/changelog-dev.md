fc13915219c2e6e113deed88f112960ef9fa505a
jordivallsq <151619025+jordivallsq@users.noreply.github.com>
2024-08-09 10:11:12 +0200

    Added loopbacks on octaves runcard (#770)
    * Added loopbacks on octaves runcard
    
    * added logs
    
    * Added loopback on tests
    
    * corrected mistakes
    
    * corrected shape of loopback
    
    * fixed code structure
    
    * added loopbacks

--- docs/releases/changelog-dev.md
+++ docs/releases/changelog-dev.md
@@ -79,6 +79,25 @@
 
   [#768](https://github.com/qilimanjaro-tech/qililab/pull/768)
 
+- Added loopbacks in the octave config file for qua following the documentation at https://docs.quantum-machines.co/1.2.0/qm-qua-sdk/docs/Guides/octave/?h=octaves#setting-the-octaves-clock. By default only port 1 of the octave is linked with a local demodulator, to work with the rest of the ports at the back ports must be connected based on the Octave Block Diagram [https://docs.quantum-machines.co/1.2.0/qm-qua-sdk/docs/Hardware/octave/#octave-block-diagram]. Where `Synth` is one of the possible 3 synths and `Dmd` is one of the 2 demodulators.
+
+  Example:
+
+    ```
+    - name: quantum_machines_cluster
+        alias: QMM
+        ...
+        octaves:
+          - name: octave1
+            port: 11252
+            ...
+            loopbacks:
+              Synth: Synth2 # Synth1, Synth2, Synth3
+              Dmd: Dmd2LO # Dmd1LO, Dmd2LO
+    ```
+
+  [#770](https://github.com/qilimanjaro-tech/qililab/pull/770)
+
 ### Improvements
 
 - Now platform.get_parameter works for QM without the need of connecting to the machine.
--- tests/data.py
+++ tests/data.py
@@ -1548,6 +1548,7 @@ class SauronQuantumMachines:
                 "name": "octave1",
                 "port": 11555,
                 "connectivity": {"controller": "con1"},
+                "loopbacks": {"Synth": "Synth2", "Dmd": "Dmd2LO"},
                 "rf_outputs": [
                     {"port": 1, "lo_frequency": 6e9},
                     {"port": 2, "lo_frequency": 6e9},
@@ -1642,6 +1643,7 @@ class SauronQuantumMachines:
                 ],
                 "rf_inputs": [{"port": 1, "lo_frequency": 6e9}, {"port": 2, "lo_frequency": 6e9}],
                 "if_outputs": [{"controller": "con1", "port": 1}, {"controller": "con1", "port": 2}],
+                "loopbacks": {"Synth": "Synth2", "Dmd": "Dmd2LO"},
             }
         ],
         "elements": [
@@ -1699,6 +1701,7 @@ class SauronQuantumMachines:
                 "name": "octave1",
                 "port": 11555,
                 "connectivity": {"controller": "con1", "fem": 1},
+                "loopbacks": {"Synth": "Synth2", "Dmd": "Dmd2LO"},
                 "rf_outputs": [
                     {"port": 1, "lo_frequency": 6e9},
                     {"port": 2, "lo_frequency": 6e9},
