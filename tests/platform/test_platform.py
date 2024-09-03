448b51d29eba789fb1f8944c69c34f91ed6bab19
jordi.valls@qilimanjaro.tech <jordi.valls@qilimanjaro.tech>
2024-09-03 08:14:25 +0000

    removed unused import

--- tests/platform/test_platform.py
+++ tests/platform/test_platform.py
@@ -25,7 +25,7 @@ from qililab.instruments.qblox import QbloxModule
 from qililab.instruments.quantum_machines import QuantumMachinesCluster
 from qililab.platform import Bus, Buses, Platform
 from qililab.pulse import Drag, Pulse, PulseEvent, PulseSchedule, Rectangular
-from qililab.qprogram import Calibration, QProgram, QuantumMachinesCompiler
+from qililab.qprogram import Calibration, QProgram
 from qililab.result.qblox_results import QbloxResult
 from qililab.result.qprogram.qprogram_results import QProgramResults
 from qililab.result.qprogram.quantum_machines_measurement_result import QuantumMachinesMeasurementResult
