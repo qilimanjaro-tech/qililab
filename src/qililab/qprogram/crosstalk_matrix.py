02fc5a261987cb8676d1b3c6abff29858e0702ba
victor <visangim@gmail.com>
2024-09-17 17:56:36 +0200

    remove unused import

--- src/qililab/qprogram/crosstalk_matrix.py
+++ src/qililab/qprogram/crosstalk_matrix.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Collection
-
 import numpy as np
 
 from qililab.yaml import yaml
