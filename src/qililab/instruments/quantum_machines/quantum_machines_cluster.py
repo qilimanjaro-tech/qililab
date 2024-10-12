7844cd513c132726fe9b7fd9d50cf20edcb2c991
Vyron Vasileiadis <hi@fedonman.com>
2024-10-12 18:16:09 +0200

    mypy fixes

--- src/qililab/instruments/quantum_machines/quantum_machines_cluster.py
+++ src/qililab/instruments/quantum_machines/quantum_machines_cluster.py
@@ -19,7 +19,15 @@ from dataclasses import dataclass
 from typing import Any, cast
 
 import numpy as np
-from qm import DictQuaConfig, Program, QmJob, QuantumMachine, QuantumMachinesManager, SimulationConfig
+from qm import (
+    DictQuaConfig,
+    OPX1000ControllerConfigType,
+    Program,
+    QmJob,
+    QuantumMachine,
+    QuantumMachinesManager,
+    SimulationConfig,
+)
 from qm.api.v2.job_api import JobApi
 from qm.jobs.running_qm_job import RunningQmJob
 from qm.octave import QmOctaveConfig
@@ -483,7 +491,7 @@ class QuantumMachinesCluster(Instrument):
         for element in elements:
             self._qm.calibrate_element(element)
 
-    def get_controller_type_from_bus(self, bus: str) -> str | None:
+    def get_controller_type_from_bus(self, bus: str) -> str:
         """Gets the OPX controller name of the bus used
 
         Args:
@@ -825,12 +833,12 @@ class QuantumMachinesCluster(Instrument):
             opx_type = self.get_controller_type_from_bus(bus=bus)
             if opx_type != "opx1000":
                 return False
-            key = "I" if "rf_inputs" in element or "mix_inputs" in element else None
+            key = "I" if ("rf_inputs" in element or "mix_inputs" in element) else None
             controller, port, fem = self.get_controller_from_element(element=element, key=key)
-            return (
-                settings_config_dict["controllers"][controller]["fems"][fem]["analog_outputs"][con_port]["output_mode"]
-                == "amplified"
+            opx1000_controller_config = cast(
+                OPX1000ControllerConfigType, settings_config_dict["controllers"][controller]
             )
+            return opx1000_controller_config["fems"][fem]["analog_outputs"][con_port]["output_mode"] == "amplified"
 
         raise ParameterNotFound(f"Could not find parameter {parameter} in instrument {self.name}")
 
