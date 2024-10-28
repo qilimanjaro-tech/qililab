96c62b3190e020c7400c27d060cd34511749483f
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2024-10-28 14:33:02 +0100

    Add basic routing test

--- tests/digital/test_circuit_router.py
+++ tests/digital/test_circuit_router.py
@@ -40,3 +40,40 @@ class TestCircuitRouter:
         assert isinstance(circuit_router.preprocessing, Preprocessing)
         assert isinstance(circuit_router.router, StarConnectivityRouter)
         assert isinstance(circuit_router.placer, StarConnectivityPlacer)
+
+    def test_route_doenst_affect_already_routed_circuit(self):
+        """Test the routing of a circuit"""
+        connectivity = nx.Graph(linear_connectivity)
+        circuit_router = CircuitRouter(connectivity)
+
+        circuit = Circuit(5)
+        circuit.add(gates.H(0))
+        circuit.add(gates.CNOT(0, 1))
+        circuit.add(gates.CNOT(1, 2))
+        circuit.add(gates.CNOT(2, 3))
+        circuit.add(gates.CNOT(3, 4))
+
+        routed_circuit, final_layout = circuit_router.route(circuit)
+
+        assert final_layout == {"q0":0, "q1":1, "q2":2, "q3":3, "q4":4}
+        assert routed_circuit.nqubits == 5
+        assert routed_circuit.depth == 5
+        assert [(gate.name, gate.qubits) for gate in routed_circuit.queue] == [(gate.name, gate.qubits)  for gate in circuit.queue]
+
+    def test_route_affects_non_routed_circuit(self):
+        """Test the routing of a circuit"""
+        connectivity = nx.Graph(star_connectivity)
+        circuit_router = CircuitRouter(connectivity)
+
+        circuit = Circuit(5)
+        circuit.add(gates.H(0))
+        circuit.add(gates.CNOT(0, 1))
+        circuit.add(gates.CNOT(1, 2))
+        circuit.add(gates.CNOT(2, 3))
+        circuit.add(gates.CNOT(3, 4))
+
+        routed_circuit, _ = circuit_router.route(circuit)
+        assert routed_circuit.nqubits == 5
+        assert routed_circuit.depth > 5
+        assert [(gate.name, gate.qubits) for gate in routed_circuit.queue] != [(gate.name, gate.qubits)  for gate in circuit.queue]
+        assert {gate.name for gate in routed_circuit.queue} >= {gate.name for gate in circuit.queue} # Assert more gates
