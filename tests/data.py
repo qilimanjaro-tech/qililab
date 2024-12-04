aee8f894ccee7a71888b43c1d5b26cb465d08fc2
jordi.valls@qilimanjaro.tech <jordi.valls@qilimanjaro.tech>
2024-12-04 09:03:09 +0000

    modified test data

--- tests/data.py
+++ tests/data.py
@@ -752,7 +752,7 @@ class SauronSpiRack:
         spi_rack_controller_wrong_module,
     ]
 
-    buses: list[dict[str, Any]] = [{RUNCARD.ALIAS: "spi_bus", RUNCARD.INSTRUMENTS: ["spi_rack"], RUNCARD.CHANNELS: [1]}]
+    buses: list[dict[str, Any]] = [{RUNCARD.ALIAS: "spi_bus", RUNCARD.INSTRUMENTS: ["S4g_1"], RUNCARD.CHANNELS: [1]}]
 
     runcard = {
         RUNCARD.NAME: name,
