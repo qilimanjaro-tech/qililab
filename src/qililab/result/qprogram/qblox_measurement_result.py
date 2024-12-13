70d8729987fa426cfc7a89b2b13c287a379be2d7
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2024-12-13 10:53:57 +0000

    reshaping

--- src/qililab/result/qprogram/qblox_measurement_result.py
+++ src/qililab/result/qprogram/qblox_measurement_result.py
@@ -51,9 +51,9 @@ class QbloxMeasurementResult(MeasurementResult):
         Returns:
             np.ndarray: The I/Q data
         """
-        path0 = self.raw_measurement_data["bins"]["integration"]["path0"].reshape(*self.shape)
-        path1 = self.raw_measurement_data["bins"]["integration"]["path1"].reshape(*self.shape)
-        return np.array([path0, path1])
+        path0 = self.raw_measurement_data["bins"]["integration"]["path0"]
+        path1 = self.raw_measurement_data["bins"]["integration"]["path1"]
+        return np.array([path0, path1]).reshape(*self.shape)
 
     @property
     def threshold(self) -> np.ndarray:
