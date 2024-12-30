675a8ebc85af2c7ebdefed36c73aabc2f1feeac2
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2024-12-30 13:15:20 +0100

    Update platform.py

--- src/qililab/platform/platform.py
+++ src/qililab/platform/platform.py
@@ -1015,7 +1015,7 @@ class Platform:
         Args:
             program (:class:`PulseSchedule` | :class:`Circuit`): Circuit or pulse schedule to execute.
             num_avg (int): Number of hardware averages used.
-            repetition_duration (int): Minimum duration of a single execution.
+            repetition_duration (int): Minimum duration of a single execution. Defaults to 200_000.
             num_bins (int, optional): Number of bins used. Defaults to 1.
             queue (Queue, optional): External queue used for asynchronous data handling. Defaults to None.
             routing (bool, optional): whether to route the circuits. Defaults to False.
