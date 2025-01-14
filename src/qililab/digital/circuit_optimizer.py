01b9376260aa1d1bd5757f2de3dca526b5199266
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2025-01-14 10:40:35 +0100

    Update circuit_optimizer.py

--- src/qililab/digital/circuit_optimizer.py
+++ src/qililab/digital/circuit_optimizer.py
@@ -186,9 +186,15 @@ class CircuitOptimizer:
 
                 # If the next gate in the same qubit is a Drag gate, we can merge them:
                 new_drag: Drag | None = cls.merge_consecutive_drags(drag1, drag2, only_same_phi)
+
+                # If successful bunching, substitute old gates with new one:
                 if new_drag is not None:
                     gate_list[idx1], drag1 = new_drag, new_drag
                     gate_list[idx2 + idx1 + 1] = None
+
+                # If not successful bunching, we can't merge this gate, go next one:
+                if new_drag is None:
+                    break
 
         # Remove None values from the list:
         return [gate for gate in gate_list if gate is not None]
