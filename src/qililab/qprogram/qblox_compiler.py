4a0c34552b2c7f0626e7ba074dbea606df829473
flavie-lebars <flavie.lebars@qilimanjaro.tech>
2025-03-14 14:56:49 +0100

    gain and freq

--- src/qililab/qprogram/qblox_compiler.py
+++ src/qililab/qprogram/qblox_compiler.py
@@ -392,7 +392,7 @@ class QbloxCompiler:
             component=QPyInstructions.SetFreq(frequency=frequency)
         )
 
-        #set the frequency a second time - because of a qblox bug where the first frequency in a hardware loop is false (same is done for the gain).
+        # #set the frequency a second time - because of a qblox bug where the first frequency in a hardware loop is false (same is done for the gain).
         self._buses[element.bus].qpy_block_stack[-1].append_component(
             component=QPyInstructions.SetFreq(frequency=frequency)
         )
@@ -422,7 +422,7 @@ class QbloxCompiler:
         self._buses[element.bus].qpy_block_stack[-1].append_component(
             component=QPyInstructions.SetAwgGain(gain_0=gain, gain_1=gain)
         )
-        #set the gain a second time - because of a qblox bug where the first gain in a hardware loop is false (same is done for the frequency).
+        # #set the gain a second time - because of a qblox bug where the first gain in a hardware loop is false (same is done for the frequency).
         self._buses[element.bus].qpy_block_stack[-1].append_component(
             component=QPyInstructions.SetAwgGain(gain_0=gain, gain_1=gain)
         )
