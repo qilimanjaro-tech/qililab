93e785c7b91350e7206d21b94590919f483d5a3e
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2025-03-25 17:58:53 +0100

    remove trailing line for solving pipy deploy

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ PACKAGE = "qililab"
 
 
 with open("src/qililab/config/version.py") as f:
-    no_empty_lines = [line.strip() for line in f.readlines() if line.strip()]
+    no_empty_lines = [line for line in f.readlines() if line.strip()]
     version = no_empty_lines[-1].split()[-1].strip("\"'")
 
 
