2e3c3cc59f696ab4483d8272f3fe78747e3e9589
jordivallsq <jordi.valls@qilimanjaro.tech>
2025-05-29 09:39:45 +0000

    fix coverage

--- tests/platform/test_platform.py
+++ tests/platform/test_platform.py
@@ -732,12 +732,7 @@ class TestMethods:
         expected_error_message = "Bus with alias 'orphan_digital_q2_flux_bus_that_does_not_exist_in_main_buses' defined in Digital/Buses section of the Runcard, not found in main Buses section of the same Runcard."
 
         with pytest.raises(ValueError, match=re.escape(expected_error_message)):
-            platform.compile(
-                program=circuit,
-                num_avg=1000,
-                repetition_duration=200_000,
-                num_bins=1
-            )
+            platform.compile(program=circuit, num_avg=1000, repetition_duration=200_000, num_bins=1)
 
     def test_compile_pulse_schedule(self, platform: Platform):
         """Test the compilation of a qibo Circuit."""
@@ -1441,6 +1436,31 @@ class TestMethods:
         assert db_real_time_saving.experiment_name == experiment_name
         assert db_real_time_saving.base_path == base_path
 
+    def test_db_real_time_saving_no_base_path(self, platform: Platform):
+        """Test db_real_time_saving function to save database from platform"""
+
+        shape = (2, 2)
+        loops = {"test_amp_loop": np.arange(0, 2)}
+        experiment_name = "test_db_real_time_saving"
+        mock_database = MagicMock()
+        db_manager = mock_database
+        optional_identifier = "optional_identifier"
+
+        drive_wf = IQPair(I=Square(amplitude=1.0, duration=40), Q=Square(amplitude=0.0, duration=40))
+        qprogram = QProgram()
+        qprogram.play(bus="drive_line_q0_bus", waveform=drive_wf)
+
+        db_real_time_saving = platform.db_real_time_saving(
+            shape=shape,
+            loops=loops,
+            experiment_name=experiment_name,
+            db_manager=db_manager,
+            qprogram=qprogram,
+            optional_identifier=optional_identifier,
+        )
+
+        assert db_real_time_saving.base_path == platform.experiment_results_base_path
+
     @patch("h5py.File")
     def test_db_save_results(self, mock_h5file, platform: Platform):
         """Test db_save_results functionto save from database from Platform"""
@@ -1482,6 +1502,33 @@ class TestMethods:
         qprogram.play(bus="drive_line_q0_bus", waveform=drive_wf)
 
         platform.db_save_results(experiment_name, results, loops, db_manager, base_path, qprogram, optional_identifier)
+
+        assert mock_h5file.called
+
+    @patch("h5py.File")
+    def test_db_save_results_no_base_path(self, mock_h5file, platform: Platform):
+        """Test db_save_results functionto save from database from Platform"""
+
+        experiment_name = "experiment_name"
+        loops = {"test_amp_loop": np.arange(0, 1)}
+        results = np.array([[1.0, 1.0], [1.0, 1.0]])
+
+        mock_database = MagicMock()
+        db_manager = mock_database
+        optional_identifier = "optional_identifier"
+
+        drive_wf = IQPair(I=Square(amplitude=1.0, duration=40), Q=Square(amplitude=0.0, duration=40))
+        qprogram = QProgram()
+        qprogram.play(bus="drive_line_q0_bus", waveform=drive_wf)
+
+        platform.db_save_results(
+            experiment_name=experiment_name,
+            results=results,
+            loops=loops,
+            db_manager=db_manager,
+            qprogram=qprogram,
+            optional_identifier=optional_identifier,
+        )
 
         assert mock_h5file.called
 
