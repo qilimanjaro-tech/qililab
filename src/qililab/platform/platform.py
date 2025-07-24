d92bffc4ff2811bc5dcd7f3de754acc04455588f
flavie-lebars <flavie.lebars@qilimanjaro.tech>
2025-07-24 17:39:35 +0200

    sphynx

--- src/qililab/platform/platform.py
+++ src/qililab/platform/platform.py
@@ -1661,7 +1661,7 @@ class Platform:
         Returns:
             data_draw (dictionary): A dictionary where keys are bus aliases and values are lists containing numpy arrays for the I and Q components. This includes all data points used
                 for plotting the waveforms. This function modifies this dictionary, it adds the offsets, the phase and the frequency to the waveforms.
-            fig (plotly object): the plotly figure of the data_draw dictionary
+
         """
         runcard_data = self._data_draw()
         qblox_draw = QbloxDraw()
