5562e32b5be7ad161fbff8283451b65e716142e6
elygoner <efarres21@gmail.com>
2025-09-25 11:10:22 +0200

    Blocker: flawed matrix algorithm

--- tests/digital/test_fuse_single_qubit_gates_pass.py
+++ tests/digital/test_fuse_single_qubit_gates_pass.py
@@ -1,6 +1,3 @@
-import math
-from typing import TYPE_CHECKING, List, Tuple
-
 from qilisdk.digital import Circuit
 from qilisdk.digital.gates import (
     CZ,
@@ -12,8 +9,8 @@ from qilisdk.digital.gates import (
     Gate,
     M,
 )
-
-from qililab.digital.circuit_transpiler_passes.circuit_transpiler_pass import CircuitTranspilerPass
+from qilisdk.digital.exceptions import GateHasNoMatrixError
+from qililab.digital.circuit_transpiler_passes.fuse_single_qubit_gates_pass import FuseSingleQubitGatesPass
 from qililab.digital.circuit_transpiler_passes.numeric_helpers import (
     _is_close_mod_2pi,
     _mat_RX,
@@ -23,6 +20,21 @@ from qililab.digital.circuit_transpiler_passes.numeric_helpers import (
     _wrap_angle,
     _zyz_from_unitary,
 )
+import pytest
+import numpy as np
 
-if TYPE_CHECKING:
-    import numpy as np
+def assert_equal_gate(list1: list[Gate], list2: list[Gate]):
+    assert len(list1) == len(list2)
+    for i, j in zip(list1, list2):
+        assert i.qubits == j.qubits
+        assert i.name == j.name
+        try:
+            assert (i.matrix == j.matrix).all()
+        except GateHasNoMatrixError:
+            pass
+
+class TestFuseSingleQubitGatesPass:
+    def test_run(self):
+        test_x = Circuit(1)
+        test_x._gates = [RX(0, theta=2)]
+        assert_equal_gate(FuseSingleQubitGatesPass().run(test_x)._gates, [RX(0,theta=2)])
