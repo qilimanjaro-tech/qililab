26ce27ff4589d2a87b189de14fb44e633a7d05a1
flavie-lebars <flavie.lebars@qilimanjaro.tech>
2025-10-06 14:19:37 +0000

    fixed the test

--- tests/result/qprogram/test_qblox_measurement_result.py
+++ tests/result/qprogram/test_qblox_measurement_result.py
@@ -18,30 +18,20 @@ def fixture_raw_measurement_data_nested() -> dict:
     """Dictionary of raw measurement data as returned from QRM instruments."""
     return {"bins": {"integration": {"path0": [1, 2, 3, 4, 5, 6], "path1": [7, 8, 9, 10, 11, 12]}, "threshold": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]}}
 
-@pytest.fixture(name="raw_measurement_data_complete")
-def fixture_raw_measurement_data_complete() -> dict:
-    """Dictionary of raw measurement data as returned from QRM instruments."""
-    return {"bins": {"integration": {"path0": [1, 2, 3, 4], "path1": [4, 5, 6, 7]}, "threshold": [0.1, 0.2, 0.3, 0.4], "avg_cnt": [0.1, 0.2, 0.3, 0.4]}, "scope":{"path0": {"data": [1, 2, 3, 4],}, "path1": {"data": [4, 5, 6, 7]}}}
-
 @pytest.fixture(name="qblox_measurement_result")
 def fixture_qblox_result_noscope(raw_measurement_data: dict):
     """Instance of QbloxMeasurementResult."""
-    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data, intertwined=1)
+    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data)
 
 @pytest.fixture(name="qblox_measurement_result_shape")
 def fixture_qblox_result_with_shape(raw_measurement_data: dict):
     """Instance of QbloxMeasurementResult."""
-    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data, shape=(1, 3), intertwined=1)
+    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data, shape=(1, 3))
 
 @pytest.fixture(name="qblox_measurement_result_nested_loops")
 def fixture_qblox_result_with_nested_loop(raw_measurement_data_nested_loops: dict):
     """Instance of QbloxMeasurementResult."""
-    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data_nested_loops, shape=(2, 3), intertwined=1)
-
-@pytest.fixture(name="qblox_measurement_result_intertwined")
-def fixture_qblox_result_intertwined(raw_measurement_data_complete: dict):
-    """Instance of QbloxMeasurementResult."""
-    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data_complete, intertwined=2)
+    return QbloxMeasurementResult(bus="readout", raw_measurement_data=raw_measurement_data_nested_loops, shape=(2, 3))
 
 
 class TestsQbloxQProgramMeasurementResult:
@@ -114,34 +104,3 @@ class TestsQbloxQProgramMeasurementResult:
         assert isinstance(thresholded_data, np.ndarray)
         assert np.all(thresholded_data == expected_thresholds)
         assert thresholded_data.shape == expected_thresholds.shape
-
-    def test_intertwined_results(self, qblox_measurement_result_intertwined: QbloxMeasurementResult):
-        """Test that the results get unintertwined for the bins"""
-
-        assert qblox_measurement_result_intertwined.intertwined == 2
-
-        unintertwined_results = qblox_measurement_result_intertwined.unintertwined
-
-        np.testing.assert_allclose(unintertwined_results[0].threshold, [0.1, 0.3])
-        np.testing.assert_allclose(unintertwined_results[1].threshold, [0.2, 0.4])
-
-        np.testing.assert_allclose(unintertwined_results[0].array, np.array([[1, 3], [4, 6]]))
-        np.testing.assert_allclose(unintertwined_results[1].array, np.array([[2, 4], [5, 7]]))
-
-        np.testing.assert_allclose(unintertwined_results[0].raw_measurement_data["bins"]["avg_cnt"], np.array([0.1, 0.3]))
-        np.testing.assert_allclose(unintertwined_results[1].raw_measurement_data["bins"]["avg_cnt"], np.array([0.2, 0.4]))
-
-        assert unintertwined_results[0].intertwined == 1
-        assert unintertwined_results[1].intertwined == 1
-
-    def test_intertwined_results_scope(self, qblox_measurement_result_intertwined: QbloxMeasurementResult):
-        """Test that the results get unintertwinedF for the scope"""
-        unintertwined_results = qblox_measurement_result_intertwined.unintertwined
-
-        np.testing.assert_allclose(unintertwined_results[0].raw_measurement_data["scope"]["path0"]["data"], np.array([1, 3]))
-        np.testing.assert_allclose(unintertwined_results[0].raw_measurement_data["scope"]["path1"]["data"], np.array([4, 6]))
-        np.testing.assert_allclose(unintertwined_results[1].raw_measurement_data["scope"]["path0"]["data"], np.array([2, 4]))
-        np.testing.assert_allclose(unintertwined_results[1].raw_measurement_data["scope"]["path1"]["data"], np.array([5, 7]))
-
-        assert unintertwined_results[0].intertwined == 1
-        assert unintertwined_results[1].intertwined == 1
