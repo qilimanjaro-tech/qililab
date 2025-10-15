fdb389816f30be8fdf972eea4ef6be995bd05eed
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2025-10-15 13:29:01 +0000

    sorting imports

--- src/qililab/instruments/becker/driver_rswu_sp16tr.py
+++ src/qililab/instruments/becker/driver_rswu_sp16tr.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Tuple, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Tuple
 
 from qcodes import VisaInstrument
 
