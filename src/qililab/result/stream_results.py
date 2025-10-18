f76bf3d1c0d214f3913c5b9f8beaefaddc66d598
jjmartinezQT <juanjo.martinez@qilimanjaro.tech>
2025-10-18 15:43:20 +0000

    unused import

--- src/qililab/result/stream_results.py
+++ src/qililab/result/stream_results.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import h5py
 import numpy as np
