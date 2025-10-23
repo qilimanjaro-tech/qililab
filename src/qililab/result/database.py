41a5be75296624b03666a68ae5c522115354e81f
OscarQili <oscar.pihl@qilimanjaro.tech>
2025-10-23 09:19:53 +0000

    fixed bug where a query was overwritten by the incorrect one

--- src/qililab/result/database.py
+++ src/qililab/result/database.py
@@ -438,24 +438,6 @@ class DatabaseManager:
                     (Measurement.debug_file != "null").label("has_debug"),
                 )
 
-            if light_read:
-                query = query.with_entities(  # Note that some columns are missing that currently are not being used
-                    Measurement.measurement_id,
-                    Measurement.experiment_name,
-                    Measurement.optional_identifier,
-                    Measurement.start_time,
-                    Measurement.end_time,
-                    Measurement.run_length,
-                    Measurement.experiment_completed,
-                    Measurement.cooldown,
-                    Measurement.sample_name,
-                    Measurement.result_path,
-                    Measurement.created_by,
-                    Measurement.debug_file,
-                    (Measurement.qprogram.isnot(None)).label("has_qprogram"),
-                    (Measurement.platform.isnot(None)).label("has_platform"),
-                )
-
             if pandas_output:
                 return read_sql(query.statement, con=con)
             return query.all()
