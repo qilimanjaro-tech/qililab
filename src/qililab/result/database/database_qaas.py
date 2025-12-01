3bcc7cb5e4430d2c64fbce163f61e4658cf9db7c
jordivallsq <151619025+jordivallsq@users.noreply.github.com>
2025-12-01 16:10:56 +0100

    Update src/qililab/result/database/database_qaas.py
    Co-authored-by: Juan José Martínez Rodríguez <133863373+jjmartinezQT@users.noreply.github.com>

--- src/qililab/result/database/database_qaas.py
+++ src/qililab/result/database/database_qaas.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Qilimanjaro Quantum Tech
+# Copyright 2025 Qilimanjaro Quantum Tech
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
