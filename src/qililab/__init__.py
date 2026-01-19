69e9c07a59a00487eb331b67b949e384753788b8
elygoner <efarres21@gmail.com>
2026-01-19 10:43:31 +0000

    All required

--- src/qililab/__init__.py
+++ src/qililab/__init__.py
@@ -20,7 +20,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("qililab")
 except PackageNotFoundError:
-    __version__ = "0.0.0"
+    __version__ = "0.31.0"
 
 import contextlib
 
