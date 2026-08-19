c66b052fd2f03c1c41ef068f4d46599830774ed3
flavie-lebars <flavie.lebars@qilimanjaro.tech>
2026-08-19 13:02:14 +0200

    mypy

--- src/qililab/analog/annealing_program.py
+++ src/qililab/analog/annealing_program.py
@@ -50,8 +50,7 @@ class AnnealingProgram:
         """Init method"""
         self._flux_to_bus_topology = flux_to_bus_topology
         self._annealing_program = annealing_program
-        # type: list # [anneal_step[chip_element_flux_line,value]]
-        self._transpiled_program = []
+        self._transpiled_program = []  # type: list # [anneal_step[chip_element_flux_line,value]]
 
     def transpile(self, transpiler: Callable):
         """First implementation of a transpiler, pretty basic but good as a first step. Transpiles from ising coefficients to fluxes
